9fb2567661a2f0e5595688138687a21c7de2f197
animBreedTools <emrkaraman@gmail.com>
2022-05-17 00:28:38 +0200

    include MME

--- src/MME.jl
+++ src/MME.jl
@@ -30,12 +30,12 @@ println("$(typeof.(f.rhs))")
 			arg1 = repr((f.rhs[i].args_parsed)[1]) #now it is Symbol
 #			arg2 = repr((f.rhs[i].args_parsed)[2]) #now it is from string
 #                	arg2 = eval(Meta.parse(arg2)) #now it is from string to data. Later will be path
-                        arg2 = userData 
-			println("arg1: $arg1 arg2: $arg2")	
+#                        arg2 = userData 
+#			println("arg1: $arg1 arg2: $arg2")	
 #                	println(ran(arg1, arg2))
 		
-                	push!(RE,ran(arg1, arg2))
-                	push!(namesRE, terms4StatsModels[i])
+#                	push!(RE,ran(arg1, arg2))
+#                	push!(namesRE, terms4StatsModels[i])
 		elseif f.rhs[i] isa FunctionTerm{typeof(|)} #to avoid schema issues/errors
 			println("$i has type | Type")
 			my_sch = schema(userData, userHints) #work on userData and userHints
