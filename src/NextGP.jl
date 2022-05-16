bce4512223f5b0b7780f4121e83ab704aa43b280
animBreedTools <emrkaraman@gmail.com>
2022-05-16 12:15:23 +0200

    import MME

--- src/NextGP.jl
+++ src/NextGP.jl
@@ -1,7 +1,7 @@
 module NextGP
 
 using DataFrames,CategoricalArrays,StatsModels,MixedModels
-import("MME.jl")
+import MME
 
 runGibbs = function(formula::FormulaTerm, userHints::Dict, userData::DataFrame)
 	return MME.mme(formula, userHints, userData)
