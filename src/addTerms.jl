b0ad4b01d643cc11a3d667c213ccb5aaa363c788
animBreedTools <emrkaraman@gmail.com>
2022-05-18 12:59:10 +0200

    include MME

--- src/addTerms.jl
+++ src/addTerms.jl
@@ -4,8 +4,10 @@ using CategoricalArrays
 
 function make_ran_matrix(x1::AbstractVector,x2::AbstractVector)
            isa(x1, CategoricalArray) ||
-                       throw(ArgumentError("ran() only works with CategoricalArrays (got $(typeof(x)))"))
-           u = unique(x2)
+                       throw(ArgumentError("ran() only works with CategoricalArrays (got $(typeof(2)))"))
+	   isa(x2, CategoricalArray) ||
+                       throw(ArgumentError("ran() only works with CategoricalArrays (got $(typeof(2)))"))
+           u = unique(x2);filter!(x->x≠0,u)
            Z = Matrix{Bool}(undef, length(x1), length(u))
            for i in eachindex(u)
                @. Z[:, i] = x1 .== u[i]
