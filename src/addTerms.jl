ab32173e2772b75670d3641810c64145a5927205
animBreedTools <emrkaraman@gmail.com>
2022-05-18 15:12:24 +0200

    include MME

--- src/addTerms.jl
+++ src/addTerms.jl
@@ -8,21 +8,27 @@ function make_ran_matrix(x1::AbstractVector,x2::AbstractVector)
 	   isa(x2, CategoricalArray) ||
                        throw(ArgumentError("ran() only works with CategoricalArrays (got $(typeof(2)))"))
            
-           filter!(x->x≠0,x1);filter!(x->x≠0,u2)
-           if length(unique(x1)) < length(unique(x2))
-		u2 = unique(x2)
-                u1 = unique(x1)
-           else
-                u2 = unique(x1)
+           filter!(x->x≠0,x1);filter!(x->x≠0,x2)
+           if length(unique(x1)) > length(unique(x2))
+		u2 = unique(x1)
                 u1 = unique(x2)
+           else
+                u2 = unique(x2)
+                u1 = unique(x1)
            end
 	  # u = unique(x2);
           # filter!(x->x≠0,u)
           # Z = Matrix{Bool}(undef, length(x1), length(u))
            Z = Matrix{Bool}(undef, length(u1), length(u2))
-           for i in eachindex(u)
+          # for i in eachindex(u)
+          #     @. Z[:, i] = x1 .== u[i]
+          # end
+          
+	  for i in eachindex(u2)
                @. Z[:, i] = x1 .== u[i]
-           end
+          end
+
+
            return Z
        end
 
