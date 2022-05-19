eed3b7ddf7726a8ba60044e74ab1d206abece5ad
animBreedTools <emrkaraman@gmail.com>
2022-05-19 09:56:14 +0200

    include MME

--- src/misc.jl
+++ src/misc.jl
@@ -1,5 +1,18 @@
-module NextGP
-
-# Write your package code here.
-
+# adapted from http://morotalab.org/Mrode2005/relmat/createA.txt
+function makeA(s::Array, d::Array)
+    n = length(s)
+    N = n + 1
+    A = zeros(N, N)
+    s = (s .== 0)*N + s
+    d = (d .== 0)*N + d
+for i in 1:n
+    A[i,i] = 1.0 + A[s[i], d[i]]/2.0
+        for j in (i+1):n
+            if j > n break end
+                A[i,j] = ( A[i, s[j]] + A[i,d[j]] )/2.0
+                A[j,i] = A[i,j]
+    end
+    end
+return(A[1:n, 1:n])
 end
+
