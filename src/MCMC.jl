488c1d5be56d29e39cf8d728788c32e1fb94f491
animBreedTools <emrkaraman@gmail.com>
2022-05-19 21:53:40 +0200

    include MME

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -18,7 +18,7 @@ runGibbs = function(formula, userHints, userData, userPedData,chainLength)
 	yVec,FE,RE,namesFE,namesRE = MME.mme(formula, userHints, userData, userPedData)
         varResidual = 350  ##### FIXED for now
         samplers.runSampler(yVec,FE,RE,varResidual,chainLength)
-        return(yVec,FE,RE)
+        return(yVec,FE,RE,namesFE)
 end
 
 end
