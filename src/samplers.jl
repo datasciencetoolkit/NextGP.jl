5c76e0ce4a7325f892cb1e85be80ec1d51e78243
animBreedTools <emrkaraman@gmail.com>
2022-05-20 14:33:53 +0200

    include MME

--- src/samplers.jl
+++ src/samplers.jl
@@ -41,7 +41,7 @@ runSampler = function(Y,X,Z,varE,chainLength,burnIn,outputFreq) ##varE will be f
         	#print
 		if iter in these2Keep
 	#		if onScreen==true
-            			@printf("iter %s b %.2f \n", iter, vcat(b...)) #i always vectorize b. maybe better to make it vector initially
+            			println("b, $(vcat(b...))") #i always vectorize b. maybe better to make it vector initially
         #		end
 		end
 	end
