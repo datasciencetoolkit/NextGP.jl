fd667430cc13aef1912fe83c1cc196f829014793
animBreedTools <emrkaraman@gmail.com>
2022-05-20 15:30:50 +0200

    print b

--- src/outFiles.jl
+++ src/outFiles.jl
@@ -5,8 +5,8 @@ export outMCMC
 using DelimitedFiles
 
 outMCMC = function(folder::String,b::Array{Float64})
-        out0 = open(pwd()*"/muOut", "a")
-        writedlm(out0, b')
+        out0 = open(pwd()*"/bOut", "a")
+        writedlm(out0, b)
         close(out0)
 end
 
