733818315fd56b3dd1f907672d1ab38bc7b2e362
animBreedTools <emrkaraman@gmail.com>
2022-05-20 12:51:50 +0200

    include MME

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -13,17 +13,10 @@ include("equations.jl")
 include("runTime.jl")
 include("samplers.jl")
 
-struct MME
-	Y
-	X::Vector{Matrix{Float64}}
-	Z::Vector{Matrix{Float64}}
-        namesX
-	namesZ	
-end	
 
 runGibbs = function(formula, userHints, userData, userPedData, chainLength, blockThese)
 	yVec,FE,RE,namesFE,namesRE = equations.mme(formula, userHints, userData, userPedData, blockThese)
-        varResidual = 35  ##### FIXED for now
+        varResidual = 350  ##### FIXED for now
         samplers.runSampler(yVec,FE,RE,varResidual,chainLength)
         return(yVec,FE,RE,namesFE,namesRE)
 end
