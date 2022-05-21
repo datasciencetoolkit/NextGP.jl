9942caa82ba92b8de7ad0f18a1eb639cd0aa42f9
animBreedTools <emrkaraman@gmail.com>
2022-05-21 17:10:13 +0200

    sample randdom effects

--- src/vcv.jl
+++ src/vcv.jl
@@ -5,14 +5,13 @@ struct ranVar
    vcov
 end
 
-end
 
 
 function sampleRanVar(νS_ranVar,effVec,df_ranVar,Str,n)
     return((νS_ranVar + effVec'*Str*effVec)/rand(Chisq(df_ranVar + n)))
 end
 
-
+end
 
 
 
