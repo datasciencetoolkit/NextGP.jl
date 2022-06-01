5c2233a50f4105c56798a3b3893a0ebfbbe7cd9b
animBreedTools <emrkaraman@gmail.com>
2022-06-01 14:28:12 +0200

    read genotypes in equations

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -17,7 +17,9 @@ include("samplers.jl")
 
 runGibbs = function(formula,userHints,userData,userPedData,nChain,nBurn,nThin,blockThese,VCV;genotypes...)
 	idY,yVec,FE,RE,GE,namesFE,namesRE,namesGE = equations.mme(formula,userHints,userData,userPedData,blockThese;paths2geno=genotypes)
+	varinfo()
         samplers.runSampler(idY,yVec,FE,RE,nChain,nBurn,nThin,VCV)
+	varinfo()
         return(yVec,FE,RE,GE,namesFE,namesRE,namesGE)
 end
 
