ddd7ea62bf3447643b2fcd34a3777a871baf69d6
animBreedTools <emrkaraman@gmail.com>
2022-06-09 10:45:21 +0200

    sample var beta

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -17,9 +17,9 @@ include("runTime.jl")
 include("samplers.jl")
 
 
-runGibbs = function(formula,userHints,userData,userPedData,nChain,nBurn,nThin,blockThese,VCV;map,genotypes...)
+runGibbs = function(formula,userHints,userData,userPedData,nChain,nBurn,nThin,blockThese,VCV,VCVM;map,genotypes...)
 	idY,yVec,FE,RE,ME,regionSizes,namesFE,namesRE,namesME = equations.mme(formula,userHints,userData,userPedData,blockThese;paths2geno=genotypes)
-        samplers.runSampler(idY,yVec,FE,RE,nChain,nBurn,nThin,VCV,ME,map,regionSizes)
+        samplers.runSampler(idY,yVec,FE,RE,nChain,nBurn,nThin,VCV,VCVM,ME,map,regionSizes)
 	println("region sizes: $regionSizes")
         return(yVec,FE,RE,ME,namesFE,namesRE,namesME)
 end
