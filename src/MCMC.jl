3d09afdf92247fda7f0b085f8841404d06d86b83
animBreedTools <emrkaraman@gmail.com>
2022-06-22 11:17:00 +0200

    userHints optional;beta' print

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -17,7 +17,7 @@ include("runTime.jl")
 include("samplers.jl")
 
 
-runGibbs = function(formula,userHints,userData,nChain,nBurn,nThin;blockThese=[],VCV=[],userPedData=[],map=[],genotypes...)
+runGibbs = function(formula,userData,nChain,nBurn,nThin;userHints=Dict{String,Any}(),blockThese=[],VCV=[],userPedData=[],map=[],genotypes...)
 	idY,A,yVec,FE,RE,ME,regionSizes = equations.mme(formula,userHints,userData,blocks=blockThese,path2ped=userPedData,paths2geno=genotypes)
         samplers.runSampler(idY,A,yVec,FE,RE,nChain,nBurn,nThin,VCV,ME,map,regionSizes)
         return(yVec,FE,RE,ME)
