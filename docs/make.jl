0dbe7395be6e483aad9c7a1bb68f024ae5c3ce62
animBreedTools <emrkaraman@gmail.com>
2022-06-24 22:25:27 +0200

    documentation made

--- docs/make.jl
+++ docs/make.jl
@@ -12,6 +12,7 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+
+deploydocs(
+    repo = "https://github.com/dataScienceToolKit/NextGP.jl"
+)
--- docs/src/index.md
+++ docs/src/index.md
@@ -3,6 +3,18 @@
 Documentation for NextGP.jl
 
 ```@docs
-make_ran_mat()
-mme()
+equations
 ```
+
+```@index
+Modules = [equations]
+Order [:make_ran_mat,:mme]
+```
+
+## Detailed API
+
+```@autodocs
+Modules = [equations]
+Order [:make_ran_mat,:mme]
+```
+
