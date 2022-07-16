408c8b2728bef55180b690e38d2877a238907e46
dataScienceTools <emrkaraman@gmail.com>
2022-07-16 18:19:44 +0200

    Doc.yml

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,20 @@
 using Documenter
 using NextGP
 
+push!(LOAD_PATH,"../src/")
 makedocs(
-         root     = "./",
-	 source   = "src",
          modules  = [NextGP],
          doctest  = false,
          clean    = true,
          sitename = "NextGP.jl",
          authors  = "Emre Karaman",
          pages = [
-            "Index" => "index.md",
+            "Home" => "index.md",
          ],
 )
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information..
 deploydocs(
-    repo = "github.com/datasciencetoolkit/NextGP.jl.git",
-    branch = "main",
+    repo = "github.com/datasciencetoolkit/NextGP.jl",
 )
