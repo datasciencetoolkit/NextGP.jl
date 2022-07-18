6d551a8b72d96a2e56a6dc9496145c2b535ac3a8
dataScienceTools <emrkaraman@gmail.com>
2022-07-18 16:29:16 +0200

    crossrefs

--- docs/src/index.md
+++ docs/src/index.md
@@ -2,8 +2,8 @@
 # NextGP.jl: Modules and Functions
 
 * `NextGP.jl` relies on several other Julia packages, which could be easily avoided and may be removed in the later releases.
-* Currently one core dependency is the [`StatsModels.jl`](https://juliastats.org/StatsModels.jl/latest/) package, for model expreession, and fixed effects design matrices generation.
-* Checking `StatsModels.jl`s manual for at least [`formula`](https://juliastats.org/StatsModels.jl/latest/formula/#The-@formula-language)  and  [`categorical data`](https://juliastats.org/StatsModels.jl/latest/contrasts/#Modeling-categorical-data) could be useful. 
+* Currently one core dependency is the [`StatsModels.jl`](https://juliastats.org/StatsModels.jl/latest/) package, for model expression, and fixed effect design matrix generation.
+* Checking `StatsModels.jl`'s manual for at least [`formula`](https://juliastats.org/StatsModels.jl/latest/formula/#The-@formula-language)  and  [`categorical data`](https://juliastats.org/StatsModels.jl/latest/contrasts/#Modeling-categorical-data) could be useful. 
 
 
 ## Public Functions
@@ -15,8 +15,6 @@ Public functions of NextGP are documented here
 mme
 makeA
 makePed
-ran
-PR
 ```
 
 ## Internals
--- src/equations.jl
+++ src/equations.jl
@@ -37,15 +37,15 @@ ranMat(arg1,arg2,data1,data2) = make_ran_matrix(data1[!,Symbol(arg1)],data2[!,Sy
 """
 	function mme(f::StatsModels.TermOrTerms, userData::DataFrame;userHints::Dict,blocks,path2ped,paths2geno)
 
-* NextGP relies on `StatsModels.jl` package for model expression (f), and fixed effect design matrix generation.
-* Details for the model expression (f), and fixed effects coding specifications (e.g., dummy or contrast coding) can be found at [`StatsModels.jl`](https://juliastats.org/StatsModels.jl/latest/).
-* Design matrices for random effects are generated either own internal functions or using StatsModels.jl "modelcols", depending on how user defined the Term in the model.
+* `NextGP` relies on `StatsModels.jl` package for model expression (`f`), and fixed effect design matrix generation.
+* Details for the model expression (`f`), and fixed effects coding specifications (e.g., dummy or contrast coding) can be found at [`StatsModels.jl`](https://juliastats.org/StatsModels.jl/latest/).
+* Design matrices for random effects are generated either own internal functions or using `StatsModels.jl`s `modelcols`, depending on how user defined the model term in the model.
 * Reads in marker data, and mean-centers the columns.
 * Finally returns lhs vector and rhs matrices.
 * By default:
-    * all Int rhs variables are made Categorical,
-    * all String rhs variables (also those made Categorical) are dummy coded, except those defined by the user in `userHints`, 
-    * all Float rhs variables are centered.
+    * all `Int` rhs variables are made `Categorical`,
+    * all `String` rhs variables (also those made `Categorical`) are dummy coded, except those defined by the user in `userHints`, 
+    * all `Float` rhs variables are centered.
 """
 function mme(f::StatsModels.TermOrTerms, userData::DataFrame;userHints::Dict,blocks,path2ped,paths2geno)
         terms4StatsModels = String.(split(repr(f.rhs), ('+')))
