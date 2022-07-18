9cf5f3be46f175b5f3b55c2df5e1d38416a06ebb
dataScienceTools <emrkaraman@gmail.com>
2022-07-18 14:37:10 +0200

    index replaced with home

--- docs/make.jl
+++ docs/make.jl
@@ -9,8 +9,8 @@ makedocs(
          sitename = "NextGP.jl",
          authors  = "Emre Karaman",
          pages = [
-            "Home" => "index.md",
-	    "Modules" => "modules.md",
+            "Home" => "modules.md",
+	    "Modules" => "index.md",
 	    "Internals" => "internals.md",
 	    "Examples" => ["Example1" => "example1/example1.md",
 			   "Example2" => "example2/example2.md",
