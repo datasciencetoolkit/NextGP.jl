318678170a49712b632f34274dab76207a4960a0
dataScienceTools <emrkaraman@gmail.com>
2022-08-30 12:02:07 +0200

    plots and names

--- src/outFiles.jl
+++ src/outFiles.jl
@@ -22,14 +22,19 @@ function outMCMC(folder::String,thisVar,output)
         close(out0)
 end
 
-function summaryMCMC(param;summary=false,outFolder=pwd()*"/outMCMC")
-        param = CSV.read("$outFolder/$(param)Out",CSV.Tables.matrix,header=false)
+function summaryMCMC(param;summary=false,plots=false,outFolder=pwd()*"/outMCMC")
+        param = CSV.read("$outFolder/$(param)Out",DataFrame,header=true)
+	namesParam = names(param)
+	param = Matrix(param)
                 if summary==true
-                        chn = Chains(param)
+                        chn = Chains(param,namesParam)
                         display(chn)
-                        display(plot(chn))
+			if plots=true
+                       		display(plot(chn))
+			end
                         param = mean(Matrix(param),dims=1)
-                else param = mean(Matrix(param),dims=1)
+                else 
+			param = mean(Matrix(param),dims=1)
                 end
         return param
 end
