11c555044a4736a7a6411fbe05aaba6f24c5d386
Emre Karaman <emrkaraman@gmail.com>
2022-10-07 11:25:43 +0200

    Update MCMC.jl

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -24,23 +24,9 @@ runGibbs = function(formula,userData,nChain,nBurn,nThin;myHints=Dict{Symbol,Any}
 
 	println("Building parts of MME")
 
-	levelsRE,levelsFE,Ainv,yVec,FE,RE,ME,regionSizes = equations.mme(formula,userData,userHints=myHints,blocks=blockThese,path2ped=userPedData,paths2geno=genotypes)
-
-
-	#########make MCMC output files. Maybe better to move into samplers
-	IO.outMCMC(outFolder,"b",levelsFE)
-
-        for i in 1:length(levelsRE)
-		nameRE = hcat(vcat(collect(values(levelsRE))[i]...)...)
-		IO.outMCMC(outFolder,"u$i",nameRE)
-		IO.outMCMC(outFolder,"varU$i",[join(collect(keys(levelsRE))[i],"_")])
-	end	
+	levelsFR,Ainv,yVec,FE,RE,ME,regionSizes = equations.mme(formula,userData,userHints=myHints,blocks=blockThese,path2ped=userPedData,paths2geno=genotypes)
 	
-
-	IO.outMCMC(outFolder,"varE",["varE"])
-	##########
-	
-        samplers.runSampler(Ainv,yVec,FE,RE,nChain,nBurn,nThin,VCV,ME,map,regionSizes,outFolder)
+        samplers.runSampler(Ainv,yVec,FE,RE,levelsFR,nChain,nBurn,nThin,VCV,ME,map,regionSizes,outFolder)
 
 	#return(yVec,FE,RE,ME)
 end
