98731a2c4a0d2096de07e6326f936166ec38553c
Emre Karaman <emrkaraman@gmail.com>
2022-10-11 14:23:08 +0200

    Update equations.jl

--- src/equations.jl
+++ src/equations.jl
@@ -14,10 +14,10 @@ export mme
 
 """
 function make_ran_matrix(x1::AbstractVector,x2::AbstractVector)
-        isa(x1, CategoricalArray) ||
-                       throw(ArgumentError("ran() only works with CategoricalArrays (got $(typeof(2)))"))
-        isa(x2, CategoricalArray) ||
-                       throw(ArgumentError("ran() only works with CategoricalArrays (got $(typeof(2)))"))
+#        isa(x1, CategoricalArray) ||
+#                       throw(ArgumentError("ran() only works with CategoricalArrays (got $(typeof(2)))"))
+#        isa(x2, CategoricalArray) ||
+#                       throw(ArgumentError("ran() only works with CategoricalArrays (got $(typeof(2)))"))
 
         u = sort(unique(x2));
         filter!(x->x≠0,u)
@@ -106,8 +106,9 @@ function mme(f::StatsModels.TermOrTerms, inputData::DataFrame;userHints::Dict,pa
 		sort!(userData, :order)
 		select!(userData, Not(:order))
 		
-		#picking up new IDs (row/column number) from pedigree, and put into sire and dam in the phenotypic data 
-		userData[!,[:ID,:Sire,:Dam]] .= pedigree[[findall(pedigree.origID.==x)[] for x in userData.origID],[:ID,:Sire,:Dam]]
+		#picking up new IDs (row/column number) from pedigree, and put into sire and dam in the phenotypic data
+		userData4ran = deepcopy(userData)
+		userData4ran[!,[:ID,:Sire,:Dam]] .= pedigree[[findall(pedigree.origID.==x)[] for x in userData4ran.origID],[:ID,:Sire,:Dam]]
 		
 	end	
 
@@ -134,7 +135,7 @@ function mme(f::StatsModels.TermOrTerms, inputData::DataFrame;userHints::Dict,pa
 			push!(summarize,[arg1,"BayesPR",typeof(ME[arg1]),size(ME[arg1],2)])
                 elseif (f.rhs[i] isa FunctionTerm) && (String(nameof(f.rhs[i].forig)) == "ran")
                         sym1 = sym2 = Symbol(repr((f.rhs[i].args_parsed)[1]))
-			IDs,thisZ = ranMat(sym1, sym2, userData, pedigree)
+			IDs,thisZ = ranMat(sym1, sym2, userData4ran, pedigree)
 			RE[(sym1,sym2)] = thisZ
 			thisZ = 0
 			idRE[(sym1,sym2)] = [pedigree[findall(i.==pedigree.ID),:origID][] for i in IDs]
