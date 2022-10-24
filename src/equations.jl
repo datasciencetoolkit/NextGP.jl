c3afd8c951df36ee9c50289f819862434f1dd553
Emre Karaman <emrkaraman@gmail.com>
2022-10-24 12:36:53 +0200

    Update equations.jl

--- src/equations.jl
+++ src/equations.jl
@@ -47,7 +47,7 @@ ranMat(arg1,arg2,data1,data2) = make_ran_matrix(data1[!,arg1],data2[!,arg2])
 """
 function mme(f::StatsModels.TermOrTerms, inputData::DataFrame;userHints::Dict,path2ped,paths2geno)
 	
-	any(typeof.(terms(f)).==ConstantTerm{Int64}) == false ? ErrorException("Models without constant term are not allowed") : nothing 
+	any(typeof.(terms(f)).==ConstantTerm{Int64}) == false ? throw(ErrorException("Models without constant term are not allowed")) : nothing 
 	
         terms4StatsModels = String.(split(repr(f.rhs), ('+')))
         terms4StatsModels = replace.(terms4StatsModels, ":" => "")
