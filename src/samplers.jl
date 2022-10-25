43649bc5533506c943f7f1c7156dfd14a9549fff
Emre Karaman <emrkaraman@gmail.com>
2022-10-25 11:59:53 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -383,43 +383,43 @@ function runSampler(iA,Y,X,Z,levelDict,blocks,chainLength,burnIn,outputFreq,prio
 @showprogress 1 "MCMC progress..." for iter in 1:chainLength
 	
 		#sample residual variance
-#	       	varE = sampleVarE(dfE,scaleE,ycorr,nData)
+	       	varE = sampleVarE(dfE,scaleE,ycorr,nData)
 		
 		#sample fixed effects
 
-#		for xSet in keys(iXpX)
-#			sampleX!(xSet,X[xSet],b,iXpX[xSet],XKeyPos[xSet],ycorr,varE)
-#		end
+		for xSet in keys(iXpX)
+			sampleX!(xSet,X[xSet],b,iXpX[xSet],XKeyPos[xSet],ycorr,varE)
+		end
 	
 		#sample random effects
-#	        sampleZandZVar!(iVarStr,Z,Zp,u,zpz,uKeyPos,nColEachZ,ycorr,varE,varU,scaleZ,dfZ)	
+	        sampleZandZVar!(iVarStr,Z,Zp,u,zpz,uKeyPos,nColEachZ,ycorr,varE,varU,scaleZ,dfZ)	
 
 		#sample marker effects and variances
 	
-#	       	for mSet in keys(mpm)
-#			sampleMandMVar!(mSet,M[mSet],Mp[mSet],beta,mpm[mSet],BetaKeyPos[mSet],regionArray[mSet],nRegions[mSet],ycorr,varE,varBeta,scaleM[mSet],dfM[mSet])
-#		end
+	       	for mSet in keys(mpm)
+			sampleMandMVar!(mSet,M[mSet],Mp[mSet],beta,mpm[mSet],BetaKeyPos[mSet],regionArray[mSet],nRegions[mSet],ycorr,varE,varBeta,scaleM[mSet],dfM[mSet])
+		end
                		
         	#print
-#		if iter in these2Keep
-#			IO.outMCMC(outPut,"b",vcat(b...)') ### currently no path is provided!!!!
-#			IO.outMCMC(outPut,"varE",varE)
+		if iter in these2Keep
+			IO.outMCMC(outPut,"b",vcat(b...)') ### currently no path is provided!!!!
+			IO.outMCMC(outPut,"varE",varE)
 			
-#			for zSet in keys(uKeyPos4Print)
-#                                IO.outMCMC(outPut,"u$(uKeyPos4Print[zSet])",u[uKeyPos4Print[zSet],1:nColEachZ[zSet]]')
-#                        end
-#			for pSet in keys(zpz)
-#				IO.outMCMC(outPut,"varU$(uKeyPos[pSet])",varU[pSet]) #join values for multivariate in uKeyPos[pSet])
-#			end
+			for zSet in keys(uKeyPos4Print)
+                                IO.outMCMC(outPut,"u$(uKeyPos4Print[zSet])",u[uKeyPos4Print[zSet],1:nColEachZ[zSet]]')
+                        end
+			for pSet in keys(zpz)
+				IO.outMCMC(outPut,"varU$(uKeyPos[pSet])",varU[pSet]) #join values for multivariate in uKeyPos[pSet])
+			end
 			
-#			for mSet in keys(BetaKeyPos4Print)
-#                                IO.outMCMC(outPut,"beta$mSet",beta[BetaKeyPos4Print[mSet],:]')
-#                        end
+			for mSet in keys(BetaKeyPos4Print)
+                                IO.outMCMC(outPut,"beta$mSet",beta[BetaKeyPos4Print[mSet],:]')
+                        end
 			
-#			for pSet in keys(mpm)
-#				IO.outMCMC(outPut,"var".*String(pSet),varBeta[pSet]')
-#			end
-#		end
+			for pSet in keys(mpm)
+				IO.outMCMC(outPut,"var".*String(pSet),varBeta[pSet]')
+			end
+		end
 	end
 end
 
