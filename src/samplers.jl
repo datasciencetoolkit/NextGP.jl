222c0c2f5abd7c43ebf54ee36f45e7e570ea5691
Emre Karaman <emrkaraman@gmail.com>
2022-10-26 23:56:12 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -429,7 +429,8 @@ function runSampler(iA,Y,X,Z,levelDict,blocks,chainLength,burnIn,outputFreq,prio
 
 
 			for pSet in keys(mpm)
-				IO.outMCMC(outPut,"var$(pSet)",varBeta[pSet]')
+#				IO.outMCMC(outPut,"var$(pSet)", varBeta[pSet]')
+				IO.outMCMC(outPut,"var$(pSet)",hcat(reduce(hcat,varBeta[pSet])...))
 			end
 		end
 	end
