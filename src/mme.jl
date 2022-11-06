3bdc380ebc20424d478a5371e9a08ed3bcfdeea6
dataScienceTools <emrkaraman@gmail.com>
2022-11-06 18:38:51 +0100

    new file functions.jl

--- src/mme.jl
+++ src/mme.jl
@@ -354,8 +354,11 @@ function getMME!(iA,Y,X,Z,M,levelDict,blocks,priorVCV,paths2maps,outPut)
 	push!(summarize,[mSet,"Random (Marker)",str,dfM[mSet],scaleM[mSet]])
 	end
 
+	println("BayesX in mme1: $BayesX")
+
 	BayesX = Dict(v==BayesPRType ? k => sampleBayesPR! : k => v for (k,v) in BayesX)
 
+	println("BayesX in mme2: $BayesX")
 
 	push!(summarize,["e","Random",priorVCV[:e].str,dfE,scaleE])						
 
