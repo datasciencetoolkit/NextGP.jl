55e1ad44c85b332d90ced3ac8882ae579d54d4e7
dataScienceTools <emrkaraman@gmail.com>
2022-11-07 13:43:52 +0100

    summaryStat

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -25,9 +25,9 @@ runLMEM = function(formula,userData,nChain,nBurn,nThin;myHints=Dict{Symbol,Any}(
 
 	levelsFR,Ainv,yVec,X,Z,M,map = prepMatVec.prep(formula,userData,userHints=myHints,path2ped=userPedData)
 
-	ycorr,nData,dfE,scaleE,X,iXpX,XKeyPos,b,Z,iVarStr,Zp,zpz,uKeyPos,uKeyPos4Print,nColEachZ,u,varU,scaleZ,dfZ,M,Mp,mpm,BetaKeyPos,BetaKeyPos4Print,beta,regionArray,nRegions,varBeta,scaleM,dfM,BayesX = mme.getMME!(Ainv,yVec,X,Z,M,levelsFR,blockThese,VCV,summaryStat,map,outFolder)
+	ycorr,nData,dfE,scaleE,X,iXpX,XKeyPos,b,Z,iVarStr,Zp,zpz,uKeyPos,uKeyPos4Print,nColEachZ,u,varU,scaleZ,dfZ,M,Mp,mpm,BetaKeyPos,BetaKeyPos4Print,beta,regionArray,nRegions,varBeta,scaleM,dfM,BayesX,rhsX,rhsZ,rhsM = mme.getMME!(Ainv,yVec,X,Z,M,levelsFR,blockThese,VCV,summaryStat,map,outFolder)
 	
-	samplers.runSampler!(ycorr,nData,dfE,scaleE,X,iXpX,XKeyPos,b,Z,iVarStr,Zp,zpz,uKeyPos,uKeyPos4Print,nColEachZ,u,varU,scaleZ,dfZ,M,Mp,mpm,BetaKeyPos,BetaKeyPos4Print,beta,regionArray,nRegions,varBeta,scaleM,dfM,BayesX,nChain,nBurn,nThin,outFolder)
+	samplers.runSampler!(ycorr,nData,dfE,scaleE,X,iXpX,XKeyPos,b,Z,iVarStr,Zp,zpz,uKeyPos,uKeyPos4Print,nColEachZ,u,varU,scaleZ,dfZ,M,Mp,mpm,BetaKeyPos,BetaKeyPos4Print,beta,regionArray,nRegions,varBeta,scaleM,dfM,BayesX,rhsX,rhsZ,rhsM,nChain,nBurn,nThin,outFolder)
 	
 end
 
