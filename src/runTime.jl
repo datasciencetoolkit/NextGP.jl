a5b80c865a452b1e985b16e079a145226587ad61
dataScienceTools <emrkaraman@gmail.com>
2022-11-07 02:01:21 +0100

    summaryStat

--- src/runTime.jl
+++ src/runTime.jl
@@ -17,7 +17,7 @@ SNP(name::Char,path::String;map::String="") = GenomicTerm(name,path,map)
 struct BayesPRType
     r::Int
     m::Union{Vector{Float64},Float64}
-    v::Union{Matrix{Float64},Float64}
+    v::Union{Vector{Float64},Matrix{Float64},Float64}
     name::String
 end
 
