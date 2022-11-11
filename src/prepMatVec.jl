21c5601a75593d35d46b59324a801fd69f28ca43
Emre Karaman <emrkaraman@gmail.com>
2022-11-11 15:34:56 +0100

    Update prepMatVec.jl

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -152,7 +152,7 @@ function prep(f::StatsModels.TermOrTerms, inputData::DataFrame;userHints::Dict,p
 			X[terms4StatsModels[i]] = Dict(:data=>thisX,:map=>[],:method=>"FixedEffects",:str=>[],:dims=>vcat(size(thisX)...),:levels=>coefnames(my_ApplySch)) 
 		
 			thisX = 0
-			push!(summarize,[f.rhs[i],typeof(f.rhs[i]),typeof(X[terms4StatsModels[i]]),X[terms4StatsModels[i]][:dims][2]])
+			push!(summarize,[f.rhs[i],typeof(f.rhs[i]),typeof(X[terms4StatsModels[i]]),last(X[terms4StatsModels[i]][:dims])])
                 end
         end
 
