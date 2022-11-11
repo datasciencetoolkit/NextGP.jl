8d692aeaaa6868286299818f99e3822d5dbc8fbd
Emre Karaman <emrkaraman@gmail.com>
2022-11-11 17:14:31 +0100

    Update MCMC.jl

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -25,7 +25,7 @@ runLMEM = function(formula,userData,nChain,nBurn,nThin;myHints=Dict{Symbol,Any}(
 
 	levelsFR,Ainv,iGRel,yVec,X,Z,M = prepMatVec.prep(formula,userData,userHints=myHints,path2ped=userPedData,priorVCV=VCV)
 
-	ycorr,nData,dfE,scaleE,X,iXpX,XKeyPos,b,Z,iVarStr,Zp,zpz,uKeyPos,uKeyPos4Print,nColEachZ,u,varU,scaleZ,dfZ,M,beta,varBeta,BayesX,rhsX,rhsZ = mme.getMME!(Ainv,iGRel,yVec,X,Z,M,levelsFR,blockThese,VCV,summaryStat,outFolder)
+	ycorr,nData,dfE,scaleE,X,b,Z,iVarStr,Zp,zpz,uKeyPos,uKeyPos4Print,nColEachZ,u,varU,scaleZ,dfZ,M,beta,varBeta,BayesX,rhsZ = mme.getMME!(Ainv,iGRel,yVec,X,Z,M,levelsFR,blockThese,VCV,summaryStat,outFolder)
 	
 	samplers.runSampler!(ycorr,nData,dfE,scaleE,X,iXpX,XKeyPos,b,Z,iVarStr,Zp,zpz,uKeyPos,uKeyPos4Print,nColEachZ,u,varU,scaleZ,dfZ,M,beta,varBeta,BayesX,rhsX,rhsZ,nChain,nBurn,nThin,outFolder)
 	
