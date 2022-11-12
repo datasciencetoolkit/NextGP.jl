a192997934d8d49ef6053edd5d114787fe567457
Emre Karaman <emrkaraman@gmail.com>
2022-11-12 21:46:01 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -94,6 +94,27 @@ end
 
 ##### Component-wise, seperated functions for symbol and tuple
 
+function sampleBayesPR2!(M::Dict,beta,ycorr,varE,varBeta)
+	for mSet in keys(M)
+		local rhs::Float64
+		local lhs::Float64
+		local meanBeta::Float64
+		for r in 1:M[mSet].nRegions
+			theseLoci = M[mSet].regionArray[r]
+			regionSize = length(theseLoci)
+			lambda = varE/(varBeta[mSet][r])
+			for locus in theseLoci::UnitRange{Int64}
+				BLAS.axpy!(getindex(beta[M[mSet].pos],locus),view(M[mSet].data,:,locus),ycorr)
+				rhs = (BLAS.dot(view(M[mSet].data,:,locus),ycorr)) .+ view(M[mSet].rhs,locus)
+				lhs = M[mSet].mpm[locus] + lambda
+				meanBeta = lhs\rhs
+				setindex!(beta[M[mSet].pos],sampleBeta(meanBeta, lhs, varE),locus)
+				BLAS.axpy!(-1.0*getindex(beta[M[mSet].pos],locus),view(M[mSet].data,:,locus),ycorr)
+			end
+			varBeta[mSet][r] = sampleVarBetaPR(M[mSet].scale,M[mSet].df,getindex(beta[M[mSet].pos],theseLoci),regionSize)
+		end
+	end
+end
 
 function sampleBayesPR!(mSet::Symbol,M,beta,ycorr,varE,varBeta)
 	local rhs::Float64
