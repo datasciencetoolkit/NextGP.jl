a3403efe8d08d12003f0a54d2b016d35212b288f
Emre Karaman <emrkaraman@gmail.com>
2022-11-15 01:38:04 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -141,12 +141,7 @@ end
 #sample random effects' variances (new U)
 function sampleVarU(iMat,scale_ranVar,df_ranVar,effVec)
 	n = size(iMat,2)
-	println("size: $(size(effVec))")
-		println("size: $(size(iMat))")
-	println("size: $(size(effVec*iMat*effVec'))")
-	println("size: $(size(effVec'*iMat*effVec))")
-
-	return (scale_ranVar*df_ranVar + effVec*iMat*effVec')/rand(Chisq(df_ranVar + n))
+	return (scale_ranVar*df_ranVar + effVec*iMat*effVec')[]/rand(Chisq(df_ranVar + n))
 end
 
 function sampleCoVarU(iMat,scale_ranVar,df_ranVar,effVec)
