c7a14f6e8107174fec3dca369b7efaaefaafae3c
Emre Karaman <emrkaraman@gmail.com>
2022-11-18 23:48:25 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -84,7 +84,6 @@ end
 function sampleZ!(zSet::Tuple,Z::Dict,u::Vector,ycorr::Vector{Float64},varE::Float64,varU::Dict)
         #for each random effect
 	println("sizes: $(size.(Z[zSet].data)) $(size(u[Z[zSet].pos]))")
-	println("$(size(Z[zSet].data.*eachcol.(getindex.(Ref(u),Z[zSet].pos))))")
 	ycorr .+= Z[zSet].data*u[Z[zSet].pos]
 	u[Z[zSet].pos] .= sampleU(zSet,Z,varE,varU,u,ycorr)
 	ycorr .-= Z[zSet].data*u[Z[zSet].pos]
