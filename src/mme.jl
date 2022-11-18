ddafc39ca2e21c35ec45ed6636c1ff7716662393
Emre Karaman <emrkaraman@gmail.com>
2022-11-18 18:13:37 +0100

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -21,6 +21,29 @@ export getMME!
 #define type for priorVCV to include Expression :(1|Dam)  or Symbol (:Dam)
 ExprOrSymbol = Union{Expr,Symbol,Tuple}
 
+#set up (co)variance structures
+function setVarCovStr!(zSet::ExprOrSymbol,Z::Dict,priorVCV)
+	if haskey(priorVCV,zSet)	
+		if ismissing(priorVCV[zSet].str) || priorVCV[zSet].str=="I" 
+			printstyled("prior var-cov structure for $zSet is either empty or \"I\" was given. An identity matrix will be used\n"; color = :green)
+			Z[zSet][:iVarStr] = Matrix(1.0I,Z[zSet][:dims][2],Z[zSet][:dims][2])
+		elseif priorVCV[zSet].str=="A"
+			printstyled("prior var-cov structure for $zSet is A. Computed A matrix (from pedigree file) will be used\n"; color = :green)
+			isa(zSet,Tuple) ? Z[zSet][:iVarStr] = Z[zSet[1]][:iVarStr] : Z[zSet][:iVarStr] = Z[zSet][:iVarStr]
+		elseif priorVCV[zSet].str=="G"
+                        printstyled("prior var-cov structure for $zSet is G. Computed G matrix will be used\n"; color = :green)
+			isa(zSet,Tuple) ? Z[zSet][:iVarStr] = Z[zSet[1]][:iVarStr] : Z[zSet][:iVarStr] = Z[zSet][:iVarStr]
+		else 	Z[zSet][:iVarStr] = inv(priorVCV[zSet].str)
+		end
+		varU_prior[zSet] = priorVCV[zSet].v
+	else	
+		printstyled("prior var-cov for $zSet is empty. An identity matrix will be used with mean=0 and variance=100\n"; color = :green)
+		varU_prior[zSet] = 100
+		priorVCV[zSet] = Random("I",0,100)
+		Z[zSet][:iVarStr] = Matrix(1.0I,Z[zSet][:dims][2],Z[zSet][:dims][2])
+	end
+end
+
 
 #main sampler
 function getMME!(Y,X,Z,M,blocks,priorVCV,summaryStat,outPut)
@@ -199,28 +222,6 @@ function getMME!(Y,X,Z,M,blocks,priorVCV,summaryStat,outPut)
 		setVarCovStr!(zSet,Z,priorVCV,varU_prior)
 	end
 																		
-function setVarCovStr!(zSet::ExprOrSymbol,Z::Dict,priorVCV)
-	if haskey(priorVCV,zSet)	
-		if ismissing(priorVCV[zSet].str) || priorVCV[zSet].str=="I" 
-			printstyled("prior var-cov structure for $zSet is either empty or \"I\" was given. An identity matrix will be used\n"; color = :green)
-			Z[zSet][:iVarStr] = Matrix(1.0I,Z[zSet][:dims][2],Z[zSet][:dims][2])
-		elseif priorVCV[zSet].str=="A"
-			printstyled("prior var-cov structure for $zSet is A. Computed A matrix (from pedigree file) will be used\n"; color = :green)
-			isa(zSet,Tuple) ? Z[zSet][:iVarStr] = Z[zSet[1]][:iVarStr] : Z[zSet][:iVarStr] = Z[zSet][:iVarStr]
-		elseif priorVCV[zSet].str=="G"
-                        printstyled("prior var-cov structure for $zSet is G. Computed G matrix will be used\n"; color = :green)
-			isa(zSet,Tuple) ? Z[zSet][:iVarStr] = Z[zSet[1]][:iVarStr] : Z[zSet][:iVarStr] = Z[zSet][:iVarStr]
-		else 	Z[zSet][:iVarStr] = inv(priorVCV[zSet].str)
-		end
-		varU_prior[zSet] = priorVCV[zSet].v
-	else	
-		printstyled("prior var-cov for $zSet is empty. An identity matrix will be used with mean=0 and variance=100\n"; color = :green)
-		varU_prior[zSet] = 100
-		priorVCV[zSet] = Random("I",0,100)
-		Z[zSet][:iVarStr] = Matrix(1.0I,Z[zSet][:dims][2],Z[zSet][:dims][2])
-	end
-end
-
 	#df, shape, scale...															
 	
 	for zSet ∈ keys(Z)
