328c39ed37dcad4a89928ff165d49b0f67633057
Emre Karaman <emrkaraman@gmail.com>
2022-11-22 13:37:31 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -57,7 +57,7 @@ function sampleU(zSet::Tuple,Z::Dict,varE::Float64,varU::Dict,u::Vector,ycorr::V
 	iVarU = inv(varU[zSet])
 	println("size Ycorr: $(size(ycorr))")
 	for i in 1:nCol
-		println("size u: $(size(getindex.(u[Z[zSet].pos],i)))")
+		println("size u: $(size(getindex.(u[Z[zSet].pos],:,i)))")
 		println("size data: $(size(Z[zSet].data[i]))")
 		ycorr .+= Z[zSet].data[i]*getindex.(u[Z[zSet].pos],i)
 		setindex!(uVec,[0;0],:,i)
