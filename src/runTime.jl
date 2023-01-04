32834aa3c358f6db8c04071277914e23cdab8ea9
Emre Karaman <emrkaraman@gmail.com>
2023-01-04 09:04:53 +0100

    Update runTime.jl

--- src/runTime.jl
+++ src/runTime.jl
@@ -1,4 +1,8 @@
 
+DiffOrSameπ = Union{Vector{Float64},Float64}
+DiffOrSamePriorCoVar = Union{Vector{Matrix{Float64}},Vector{Float64},Matrix{Float64}
+
+
 struct RandTerm
     var::Char
 end
@@ -20,6 +24,15 @@ struct BayesPRType
 end
 
 BayesPR(r::Int,v::Union{Matrix{Float64},Float64};name="BayesPR") = BayesPRType(r,v,name)
+
+
+struct BayesBType
+    π::DiffOrSameπ          #pi can be different (vector) or same per SNP
+    v::DiffOrSamePriorCoVar #(co)var can be different (vector) or same per SNP
+    name::String
+end
+
+BayesB(π::DiffOrSameπ,v::DiffOrSamePriorCoVar;name="BayesB") = BayesBType(π,v,name)
 
 
 struct RandomEffectType
