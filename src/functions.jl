00736ad7d60cad1ff34c12c4ff5e29a070b3be6a
Emre Karaman <emrkaraman@gmail.com>
2023-01-04 07:56:20 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -129,6 +129,27 @@ function sampleBayesPR!(mSet::Tuple,M::Dict,beta::Vector,ycorr::Vector{Float64},
 	end
 end
 
+
+function sampleBayesB!(mSet::Symbol,M::Dict,beta::Vector,ycorr::Vector{Float64},varE::Float64,varBeta::Dict)
+	local rhs::Float64
+	local lhs::Float64
+	local meanBeta::Float64
+	local lambda::Float64
+	for (r,theseLoci) in enumerate(M[mSet].regionArray)
+		regionSize::Int64 = length(theseLoci)
+		lambda = varE/(varBeta[mSet][r])
+		for locus in theseLoci::UnitRange{Int64}
+			BLAS.axpy!(getindex(beta[M[mSet].pos],locus),view(M[mSet].data,:,locus),ycorr)
+			rhs = BLAS.dot(view(M[mSet].data,:,locus),ycorr) + getindex(M[mSet].rhs,locus)
+			lhs = getindex(M[mSet].mpm,locus) + lambda
+			meanBeta = lhs\rhs
+			setindex!(beta[M[mSet].pos],sampleBeta(meanBeta, lhs, varE),locus)
+			BLAS.axpy!(-1.0*getindex(beta[M[mSet].pos],locus),view(M[mSet].data,:,locus),ycorr)
+		end
+		@inbounds varBeta[mSet][r] = sampleVarBetaPR(M[mSet].scale,M[mSet].df,getindex(beta[M[mSet].pos],theseLoci),regionSize)
+	end
+end
+
 #####
 
 
