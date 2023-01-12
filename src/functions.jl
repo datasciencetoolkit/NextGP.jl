6420910681f5807d6dea9b182746899ea67abe34
Emre Karaman <emrkaraman@gmail.com>
2023-01-12 13:19:19 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -201,7 +201,7 @@ function sampleBayesR!(mSet::Symbol,M::Dict,beta::Vector,delta::Vector,ycorr::Ve
 	local lhs::Float64
 	local meanBeta::Float64
 	local lambda::Float64
-	nLoci = zeros(length(M[mSet].nVarCov))
+	nLoci = zeros(M[mSet].nVarCov)
 	println("nLoci: $nLoci")
 	for (r,theseLoci) in enumerate(M[mSet].regionArray) #theseLoci is always as 1:1,2:2 for BayesR
 		lambda = varE/(varBeta[mSet][r])
