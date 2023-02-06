827afa95cab7219749eb9abeba49d4b9fb0badf0
Emre Karaman <emrkaraman@gmail.com>
2023-02-06 10:44:11 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -257,7 +257,7 @@ function sampleBayesLV!(mSet::Symbol,M::Dict,beta::Vector,delta::Vector,ycorr::V
 	end
 	
 	# model variance
-	var_var = 0.0001
+	var_var = 0.01
 	trapped = 0
 	notTrapped = 0
 	for (r,theseLoci) in enumerate(M[mSet].regionArray) #theseLoci is always as 1:1,2:2 for BayesB, so r=locus
