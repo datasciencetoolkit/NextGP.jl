eb55f6300b6794e3dd4a6ec703b1eef49ba46863
Emre Karaman <emrkaraman@gmail.com>
2023-04-12 14:25:14 +0200

    Update runTime.jl

--- src/runTime.jl
+++ src/runTime.jl
@@ -80,15 +80,17 @@ struct BayesRType
     class::Vector{Float64}
     v::VarCovarTypes
     name::String
+    estimatePi::Bool
 end
 
 """
         function BayesR(pi,class,v)
-* `pi` is the vector of proportion of SNPs for each variance class.
+* `pi` is the vector of proportion of SNPs for each variance class. If `estimatePi=true`, it is only used as a starting value.
 * `class` is the vector of scales of common SPN variance for each variance class. The scales should be in the increasing order. For example, [0.0,0.0001,0.001,0.01].
 * `v` is the variance for the prior distribution of SNPs.
+* `estimatePi` is `true`if `pi` is estimated. By default it is ´false´
 """
-BayesR(pi::PiTypes,class::Vector{Float64},v::VarCovarTypes;name="BayesR") = BayesRType(pi,class,v,name)
+BayesR(pi::PiTypes,class::Vector{Float64},v::VarCovarTypes;name="BayesR",estimatePi::Bool=false) = BayesRType(pi,class,v,name,estimatePi)
 
 struct BayesLogVarType
     v::Union{Matrix{Float64},Float64}
