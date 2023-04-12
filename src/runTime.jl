cdc91d81522ab2c57879338969a9d699c3f6392c
Emre Karaman <emrkaraman@gmail.com>
2023-04-12 14:25:42 +0200

    Update runTime.jl

--- src/runTime.jl
+++ src/runTime.jl
@@ -84,7 +84,7 @@ struct BayesRType
 end
 
 """
-        function BayesR(pi,class,v)
+        function BayesR(pi,class,v;estimatePi=false)
 * `pi` is the vector of proportion of SNPs for each variance class. If `estimatePi=true`, it is only used as a starting value.
 * `class` is the vector of scales of common SPN variance for each variance class. The scales should be in the increasing order. For example, [0.0,0.0001,0.001,0.01].
 * `v` is the variance for the prior distribution of SNPs.
