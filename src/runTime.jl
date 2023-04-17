e12f1540d1c5b774bed7b68a6de7a5d6ff0e85f0
Emre Karaman <emrkaraman@gmail.com>
2023-04-17 11:59:57 +0200

    Update runTime.jl

--- src/runTime.jl
+++ src/runTime.jl
@@ -92,6 +92,24 @@ end
 """
 BayesR(pi::PiTypes,class::Vector{Float64},v::VarCovarTypes;name="BayesR",estimatePi::Bool=false) = BayesRType(pi,class,v,name,estimatePi)
 
+struct BayesRCType
+    pi::PiTypes
+    class::Vector{Float64}
+    v::VarCovarTypes
+    name::String
+    estimatePi::Bool
+end
+
+"""
+        function BayesRCπ(pi,class,v,annot;estimatePi=false)
+* `pi` is the vector of proportion of SNPs for each variance class. If `estimatePi=true`, it is only used as a starting value.
+* `class` is the vector of scales of common SPN variance for each variance class. The scales should be in the increasing order. For example, [0.0,0.0001,0.001,0.01].
+* `v` is the variance for the prior distribution of SNPs.
+* `annnot` is a matrix of annottations. Rows are SNPs, columnns are annnotation classes. Entries are binary coded, 0/1.
+* `estimatePi` is `true`if `pi` is estimated. By default it is ´false´
+"""
+BayesRCπ(pi::PiTypes,class::Vector{Float64},v::VarCovarTypes,annot::Matrix{Int64};name="BayesRCπ",estimatePi::Bool=false) = BayesRCType(pi,class,v,annot,name,estimatePi)
+
 struct BayesLogVarType
     v::Union{Matrix{Float64},Float64}
     f::StatsModels.TermOrTerms
