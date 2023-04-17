401c7954e5c7543a69315e024cf6e5c65263029d
Emre Karaman <emrkaraman@gmail.com>
2023-04-17 12:46:34 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -340,6 +340,7 @@ function getMME!(Y,X,Z,M,blocks,priorVCV,summaryStat,outPut)
 				M[pSet][:nVarCov] = 1 #length(priorVCV[pSet].pi)
 				M[pSet][:estPi]       = priorVCV[pSet].estimatePi
 				M[pSet][:piHat]       = deepcopy(priorVCV[pSet].pi)
+				M[pSet][:annotProb]   = priorVCV[pSet].annot./sum(priorVCV[pSet].annot,dims=2)				
 			elseif priorVCV[pSet].name == "BayesLV"
 				M[pSet][:method]      = "BayesLV"
 				M[pSet][:funct]       = sampleBayesLV!
