6451f495183a69d37cc691dd347fe4c38d6bd841
Emre Karaman <emrkaraman@gmail.com>
2023-04-18 08:47:01 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -520,7 +520,7 @@ function getMME!(Y,X,Z,M,blocks,priorVCV,summaryStat,outPut)
 			end
 			if in(M[mSet][:method],["BayesRCπ"])
 				npis = length(M[mSet][:vClass])*M[mSet][:nVarCov]
-				IO.outMCMC(outPut,"pi$mSet",[["pi$v" for v in 1:npis]]) #[] to have it as one row
+				IO.outMCMC(outPut,"annot$mSet",[["annot$v" for v in 1:npis]]) #[] to have it as one row
 			end
 		elseif isa(mSet,Tuple)
 			for m in mSet
