fb655f5f0bf3f622ab2adae7d6cc6d4cf4d3686d
Emre Karaman <emrkaraman@gmail.com>
2023-06-26 12:25:52 +0200

    Update functions.jl for mme

--- src/functions.jl
+++ src/functions.jl
@@ -19,11 +19,9 @@ export sampleZandZVar!
 function sampleX!(xSet::Union{Symbol,Tuple},X::Dict,b::Vector,ycorr::Vector,varE::Float64)
 	iVarE = inv(varE)
 	if length(b[X[xSet].pos])==1
-		println("X Set lhs: $(X[xSet].lhs) , XSet rhs: $(X[xSet].rhs)")
 		ycorr    .+= X[xSet].data .* b[X[xSet].pos]
 		rhs      = (X[xSet].data'*ycorr).*iVarE .+ X[xSet].rhs
 		lhs      = X[xSet].xpx .*iVarE .+ X[xSet].lhs
-		println("lhs: $lhs , rhs: $rhs")
 		meanMu   = lhs\rhs			
                 b[X[xSet].pos] .= rand(Normal(meanMu[],sqrt(inv(lhs[]))))
 		ycorr    .-= X[xSet].data .* b[X[xSet].pos]
