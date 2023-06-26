66b9848434602b00e3d6c6f548f632b7b5049312
Emre Karaman <emrkaraman@gmail.com>
2023-06-26 14:27:46 +0200

    unzip E

--- src/mme.jl
+++ src/mme.jl
@@ -547,7 +547,8 @@ function getMME!(Y,X,Z,M,blocks,priorVCV,summaryStat,outPut)
 	
 	X  = myUnzip(X)
 	Z  = myUnzip(Z)
-	M  = myUnzip(M)	
+	M  = myUnzip(M)
+	E  = myUnzip(E)
 	
 	return ycorr, nData, E, X, b, Z, u, varU, M,  beta, varBeta, delta
 	
