f8e8f7ce5b236f50ff1dfdd39c05d4d460f48793
Emre Karaman <emrkaraman@gmail.com>
2023-06-29 11:58:04 +0200

    Union{Matrix{Float64},String} for SNP

--- src/runTime.jl
+++ src/runTime.jl
@@ -19,13 +19,13 @@ end
 """
         function SNP(name,path;map="")
 * Defines SNP information for further analysis.
-* `path` is the path for the marker file.
+* `path` is the path for the marker file, or the matrix for the marker genotypes.
 * Marker files are currently expected to be ordered as the phenotype data.
 * The method to be applied to the data, `GBLUP`, `BayesB`, `BayesR` etc, is defined in the prior setting.
 * Map file is optional. If not provided, a Bayesian Regression model with common variance for all SNPs will be applied. If provided, shoul match the order in the genotype file.
 * One most avoid overlapping marker sets by using different `name`s.
 """
-SNP(name::Char,path::String;map::String="") = GenomicTerm(name,path,map)
+SNP(name::Char,path::Union{Matrix{Float64},String};map::String="") = GenomicTerm(name,path,map)
 
 struct BayesPRType
     r::Int
