906a35096f448bdce92786658f0d5b5b4bcda221
Emre Karaman <emrkaraman@gmail.com>
2023-07-03 12:22:18 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -126,7 +126,11 @@ function sampleBayesPR!(mSet::Symbol,M::Dict,beta::Vector,delta::Vector,ycorr::V
 	for (r,theseLoci) in enumerate(M[mSet].regionArray)
 		regionSize::Int64 = length(theseLoci)
 		iVarBeta = 1/varBeta[mSet][r]
+		println("Mp: $M[mSet].Mp")
+		println("Mp: $M[mSet].mpm")
+		println("Mp: $(size(M[mSet].Mp))")
 		for locus in theseLoci::UnitRange{Int64}
+			println("getindex(M[mSet].Mp,locus): $(getindex(M[mSet].Mp,locus))")
 			BLAS.axpy!(getindex(beta[M[mSet].pos],locus),view(M[mSet].data,:,locus),ycorr)
 			rhs = getindex(M[mSet].Mp,locus)*ycorr.*iVarE + getindex(M[mSet].rhs,locus)
 			lhs = getindex(M[mSet].mpm,locus)*iVarE + getindex(M[mSet].lhs,locus) + iVarBeta
