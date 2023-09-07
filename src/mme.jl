24f7ea7df598c6e05106d694920da64ee6570e14
Emre Karaman <emrkaraman@gmail.com>
2023-09-07 12:56:30 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -147,7 +147,7 @@ function getMME!(Y,X,Z,M,blocks,priorVCV,summaryStat,outPut)
                 end
 
 		if isa(X[xSet][:xpx],Matrix{Float64}) 
-			X[xSet][:xpx] += Matrix(I*minimum(abs.(diag(X[xSet][:xpx])./100)),size(X[xSet][:xpx]))
+			X[xSet][:xpx] += Matrix(I*minimum(abs.(diag(X[xSet][:xpx])./10)),size(X[xSet][:xpx]))
 		end
         end
 
