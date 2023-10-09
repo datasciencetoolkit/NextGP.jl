55a743586ec19b8bcddb313918a29569715a1d6e
Emre Karaman <emrkaraman@gmail.com>
2023-10-09 11:52:51 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -88,7 +88,7 @@ function runSampler!(ycorr,nData,E,X,b,Z,u,varU,M,beta,varBeta,delta,chainLength
 					end
 					if in(M[mSet].method,["BayesLV"])
 						IO.outMCMC(outPut,"c$mSet",[vcat(M[mSet].c...)])
-						IO.outMCMC(outPut,"eta$mSet",M[mSet].eta)
+						IO.outMCMC(outPut,"zeta$mSet",M[mSet].zeta)
 					end
 				elseif isa(mSet,Tuple)
 					for p in M[mSet].pos
