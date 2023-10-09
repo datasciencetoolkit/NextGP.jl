08764f462c3ee3bfa9076e9f3963f4c15572f775
Emre Karaman <emrkaraman@gmail.com>
2023-10-09 09:58:29 +0200

    BayesLV print

--- src/mme.jl
+++ src/mme.jl
@@ -582,6 +582,8 @@ function getMME!(Y,X,Z,M,blocks,priorVCV,summaryStat,outPut)
 				npis = length(M[mSet][:vClass])*M[mSet][:nVarCov]
 				IO.outMCMC(outPut,"pi$mSet",[["pi$v" for v in 1:npis]]) #[] to have it as one row
 				IO.outMCMC(outPut,"annot$mSet",hcat(M[mSet][:levels]...))
+			elseif in(M[mSet][:method],["BayesLV"])
+				IO.outMCMC(outPut,"pi$mSet",[["q$v" for v in 1:(length(M[pSet][:c]))]]) #[] to have it as one row
 			end
 		elseif isa(mSet,Tuple)
 			for m in mSet
