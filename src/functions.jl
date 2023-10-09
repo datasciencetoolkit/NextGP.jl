095b339e59805576a2395b44e06437bfc23eea98
Emre Karaman <emrkaraman@gmail.com>
2023-10-09 12:59:05 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -478,7 +478,7 @@ function sampleBayesLV!(mSet::Symbol,M::Dict,beta::Vector,delta::Vector,ycorr::V
 #	var_var = sampleVarE(0,0.0,M[mSet].SNPVARRESID,length(M[mSet].SNPVARRESID)) #var(M[mSet].SNPVARRESID) #0.01
 	var_var = M[mSet].estZeta == true ? var(M[mSet].SNPVARRESID) : M[mSet].zeta
 
-	M[mSet].zeta[1] = var_var
+	setindex!(M[mSet].zeta,var_var,1)
 	
 	nLoci = 0
 	iVarE = 1/varE
