64a8f5e4edd0b5d5c69b2cc6fd33b6bd95e146ea
dataScienceTools <emrkaraman@gmail.com>
2023-10-24 10:56:01 +0200

    convenience functions MCMC

--- src/misc.jl
+++ src/misc.jl
@@ -235,4 +235,19 @@ myUnzip(d::Dict) = Dict(p.first => (;p.second...) for p in d)
 myUnzip(d) = d
 
 
-
+function summaryMCMC(param;summary=false,plots=false,outFolder=pwd()*"/outMCMC")
+        param = CSV.read("$outFolder/$(param)Out",DataFrame,header=true)
+        namesParam = names(param)
+        param = Matrix(param)
+                if summary==true
+                        chn = Chains(param,namesParam)
+                        display(chn)
+                        if plots==true
+                                display(plot(chn))
+                        end
+                        param = mean(Matrix(param),dims=1)
+                else
+                        param = mean(Matrix(param),dims=1)
+                end
+        return param
+end
