05b232186743623312acaf1a4a81d3c1fbe9f327
Emre Karaman <emrkaraman@gmail.com>
2024-03-22 15:12:34 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -528,10 +528,7 @@ function sampleBayesLV!(mSet::Symbol,M::Dict,beta::Vector,delta::Vector,ycorr::V
 	M[mSet].SNPVARRESID .+= M[mSet].covariates*M[mSet].c			
 	rhsC = M[mSet].covariatesT*M[mSet].SNPVARRESID	
 	meanC   = M[mSet].iCpC*rhsC
-	##
-	M[mSet].c .= meanC
-	##
-#	M[mSet].c .= rand(MvNormal(vec(meanC),convert(Array,Symmetric(M[mSet].iCpC*var_var))))
+	M[mSet].c .= rand(MvNormal(vec(meanC),convert(Array,Symmetric(M[mSet].iCpC*var_var))))
 	M[mSet].SNPVARRESID .-= M[mSet].covariates*M[mSet].c	
 end
 
