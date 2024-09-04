7d6aa13315326c9a203cef4d284577c6b7cb48e6
Emre Karaman <emrkaraman@gmail.com>
2024-09-04 11:29:16 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -287,9 +287,11 @@ function getMME!(Y,X,Z,M,blocks,priorVCV,summaryStat,outPut)
 	for pSet in keys(M) 		#keys(filter(p -> p.first!=:e, priorVCV)) # excluding :e keys(priorVCV)
 		println("pSet now: $pSet")
 #		in(pSet, collect(keys(M))[(!in).(keys(M),Ref(keys(priorVCV)))]) ? throw(ArgumentError("You must provide a prior for genomic analysis. Example: BayesPR(9999,0.05)")) : nothing
-
+		
+		haskey(priorVCV,pSet) ? nothing: printstyled("No prior was provided for $pSet, but it was included in the data. It will be made uncorrelated with default priors and region size 9999 (WG)\n"; color = :green)
+		
 		#symbol :M1 or expression
-		if isa(pSet,Symbol) && in(pSet,keys(M))
+		if isa(pSet,Symbol) #&& in(pSet,keys(M)), now keys are always in keys(M)
 			posMcounter += 1
 			M[pSet][:pos] = posMcounter
 			tempmpm = []
