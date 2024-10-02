f9180c2bd33e6b06de02afa568ac6746ce30642a
Emre Karaman <emrkaraman@gmail.com>
2024-10-02 22:41:45 +0200

    Update prepMatVec.jl

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -6,8 +6,9 @@ using StatsModels, MixedModels, CategoricalArrays, CSV, StatsBase, DataStructure
 
 #import StatsModels.terms
 #StatsModels.terms!(path::String) = path #path for data and map
-import StatsModels.term
-StatsModels.term(path::String) = path #path for data and map
+import StatsModels.parse!
+parse!(path::String, protected) = path
+StatsModels.termvars(path::String) = path #path for data and map
 
 include("misc.jl")
 
