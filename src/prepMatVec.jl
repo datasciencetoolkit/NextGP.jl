0dfdaa176d3c974b76f030709469d8fa7b9eeec7
Emre Karaman <emrkaraman@gmail.com>
2024-10-03 12:28:12 +0200

    Update prepMatVec.jl

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -10,13 +10,6 @@ import StatsModels.parse!
 parse!(path::String, protected) = path
 StatsModels.termvars(path::String) = path #path for data and map
 
-#display the term for my custom functions correctly
-function Base.show(io::IO, ::MIME"text/plain",
-                   t::FunctionTerm{typeof(SNP)};
-                   prefix = "")
-    print(io, prefix, "(")
-    print(io,first(t.args), ")->", t.exorig)
-end
 
 include("misc.jl")
 
