32c6956dbb3af68a041fa1d71f3d86ec539424c6
Emre Karaman <emrkaraman@gmail.com>
2024-10-07 12:11:53 +0200

    Update misc.jl

--- src/misc.jl
+++ src/misc.jl
@@ -12,9 +12,9 @@ parse!(path::String, protected) = path
 StatsModels.termvars(path::String) = path #path for data and map
 
 #display the term for my custom functions correctly
-#Base.show(io::IO, t::FunctionTerm{typeof(SNP)}) = print(io, ":($(t.exorig))")
+Base.show(io::IO, t::FunctionTerm) = print(io, ":($(t.exorig))")
 function Base.show(io::IO, ::MIME"text/plain",
-                   t::FunctionTerm{typeof(SNP)};
+                   t::FunctionTerm;
                    prefix = "")
     print(io, prefix, "(")
     print(io,first(t.args), ")->", t.exorig)
