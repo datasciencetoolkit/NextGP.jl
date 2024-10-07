5b5c38819a469fc87c69c32ca354a2588673718f
Emre Karaman <emrkaraman@gmail.com>
2024-10-07 12:15:34 +0200

    Update prepMatVec.jl

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -4,6 +4,21 @@ module prepMatVec
 
 using StatsModels, MixedModels, CategoricalArrays, CSV, StatsBase, DataStructures, DataFrames, PrettyTables, LinearAlgebra
 
+import StatsModels.parse!
+parse!(path::String, protected) = path
+StatsModels.termvars(path::String) = path #path for data and map
+
+#display the term for my custom functions correctly
+include("runTime.jl")
+Base.show(io::IO, t::FunctionTerm) = print(io, ":($(t.exorig))")
+function Base.show(io::IO, ::MIME"text/plain",
+                   t::FunctionTerm;
+                   prefix = "")
+    print(io, prefix, "(")
+    print(io,first(t.args), ")->", t.exorig)
+end
+
+
 include("misc.jl")
 
 export prep
