8f186334ffe3f5280a06c6888c21400cbdb0bb49
dataScienceTools <emrkaraman@gmail.com>
2024-10-10 13:53:29 +0200

    reexport

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -4,8 +4,7 @@ module prepMatVec
 
 using StatsModels, MixedModels, CategoricalArrays, CSV, StatsBase, DataStructures, DataFrames, PrettyTables, LinearAlgebra
 
-using Reexport
-@reexport import StatsModels.parse!
+import StatsModels.parse!
 parse!(path::String, protected) = path
 StatsModels.termvars(path::String) = path #path for data and map
 
