8d5dd4d9435a517b7cfe40458d2be6a4704ddae7
Emre Karaman <emrkaraman@gmail.com>
2025-01-24 11:04:49 +0100

    Update misc.jl

--- src/misc.jl
+++ src/misc.jl
@@ -4,6 +4,25 @@ using CSV
 using PedigreeBase
 using StatsBase
 
+preserved = [:*,:+, :~, :-, :|, :/]
+isacall(exp::Expr) = (exp.head == :call)
+#isacall(exp::Expr) = ((exp == :Symbol) && !in(exp,preserved))
+isacall(exp::Int) = false
+isacall(exp::Symbol) = false
+
+function getTerms(preserved,f)
+	modelTerms = Dict()
+	for term in filter(!in(preserved), f.rhs.args)
+		!isacall(term) && (term==1) ? modelTerms[term] = ConstantTerm(term) : nothing
+		!isacall(term) && isa(term,Symbol) ? modelTerms[term] = DataTerm(term) : nothing
+		isacall(term) && isdefined(Base, term.args[1]) && (getproperty(Main, term.args[1]) isa Function) && (getproperty(Main, term.args[1]) == *) ? modelTerms[term] = InteractionTerm(term.args[2:end]) : nothing
+		isacall(term) && isdefined(Base, term.args[1]) && (getproperty(Main, term.args[1]) isa Function) && (getproperty(Main, term.args[1]) != *) ? modelTerms[term] = FunctionTerm(term.args[1],term.args[2]) : nothing
+		isacall(term) && (term.args[1] == :PED) ? modelTerms[term.args[2]]=PedTerm(term.args[2:end]...) : nothing
+		isacall(term) && (term.args[1] == :SNP) ? modelTerms[term.args[2]]=GenomicTerm(term.args[2:end]...) : nothing	
+	end
+	return modelTerms
+end
+
 
 """
         make_ran_matrix(x1::AbstractVector,x2::AbstractVector)
