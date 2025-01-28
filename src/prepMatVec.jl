b7e43342332f695b5f035edf779c7f05aac2fcb7
Emre Karaman <emrkaraman@gmail.com>
2025-01-28 11:21:05 +0100

    Update prepMatVec.jl

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -3,6 +3,7 @@ module prepMatVec
 using CategoricalArrays, CSV, StatsBase, DataStructures, DataFrames, PrettyTables, LinearAlgebra
 
 include("types.jl")
+include("model.jl")
 include("misc.jl")
 include("designMat.jl")
 
