6cd13d16715ecb0af6e8a6c5a42cc4520d00f652
Emre Karaman <emrkaraman@gmail.com>
2025-04-14 11:36:18 +0200

    Update model.jl
    getRhsTerms

--- src/model.jl
+++ src/model.jl
@@ -19,15 +19,15 @@ isacall(exp::Expr) = (exp.head == :call)
 isacall(exp::Int) = false
 isacall(exp::Symbol) = false
 
-function getTerms(f;pre=preserved)
-	modelTerms = Dict()
+function getRhsTerms(f;pre=preserved)
+	modelRhsTerms = Dict()
 	for term in filter(!in(preserved), f.rhs.args)
-		!isacall(term) && (term==1) ? modelTerms[:(Intercept)] = ConstantTerm(term) : nothing
-		!isacall(term) && isa(term,Symbol) ? modelTerms[term] = DataTerm(term) : nothing
-		isacall(term) && isdefined(Base, term.args[1]) && (getproperty(Main, term.args[1]) isa Function) && (getproperty(Main, term.args[1]) == *) ? modelTerms[term] = InteractionTerm(term.args[2:end]) : nothing
-		isacall(term) && isdefined(Base, term.args[1]) && (getproperty(Main, term.args[1]) isa Function) && (getproperty(Main, term.args[1]) != *) ? modelTerms[term] = FunctionTerm(term.args[1],term.args[2]) : nothing
-		isacall(term) && (term.args[1] == :PED) ? modelTerms[term.args[2]]=PED(term.args[2:end]...) : nothing
-		isacall(term) && (term.args[1] == :SNP) ? modelTerms[term.args[2]]=SNP(term.args[2:end]...) : nothing	
+		!isacall(term) && (term==1) ? modelRhsTerms[:(Intercept)] = ConstantTerm(term) : nothing
+		!isacall(term) && isa(term,Symbol) ? modelRhsTerms[term] = DataTerm(term) : nothing
+		isacall(term) && isdefined(Base, term.args[1]) && (getproperty(Main, term.args[1]) isa Function) && (getproperty(Main, term.args[1]) == *) ? modelRhsTerms[term] = InteractionTerm(term.args[2:end]) : nothing
+		isacall(term) && isdefined(Base, term.args[1]) && (getproperty(Main, term.args[1]) isa Function) && (getproperty(Main, term.args[1]) != *) ? modelRhsTerms[term] = FunctionTerm(term.args[1],term.args[2]) : nothing
+		isacall(term) && (term.args[1] == :PED) ? modelRhsTerms[term.args[2]]=PED(term.args[2:end]...) : nothing
+		isacall(term) && (term.args[1] == :SNP) ? modelRhsTerms[term.args[2]]=SNP(term.args[2:end]...) : nothing	
 	end
-	return modelTerms
+	return modelRhsTerms
 end
