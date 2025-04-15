88f8394984e2850b3cd168652eba2cd47ca9bcaa
Emre Karaman <emrkaraman@gmail.com>
2025-04-15 11:50:36 +0200

    Update prepMatVec.jl

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -73,8 +73,10 @@ end
     * all `String` rhs variables (also those made `Categorical`) are dummy coded,
     * all `Float` rhs variables are centered.
 """
+println("f type: $(typeof(f))")
+
 function prep(f;path2ped=[],priorVCV=[])
-	if isa(f,lmm)
+	if isa(f,NextGP.lmm)
 		modelRHSTerms = getRHSTerms(f)
 		modelLHSTerms = getLHSTerms(f)
 		#yVec is a vector if one response variable, matrix otherwise. functions.jl may need to be changed to work with matrix yCorr also.
@@ -87,7 +89,7 @@ function prep(f;path2ped=[],priorVCV=[])
 			inputData = CSV.read(f.data,DataFrames.DataFrame,header=true,delim=',')
 			Y = hcat([makeX(inputData,k)[:data] for (k,v) in modelLHSTerms]...)
 		end
-	elseif isa(f,Tuple{Vararg{lmm}})
+	elseif isa(f,Tuple{Vararg{NextGP.lmm}})
 		modelLHSTerms = Dict()
 		for (i,fi) in enumerate(f)
 			println("reading $i $fi")
