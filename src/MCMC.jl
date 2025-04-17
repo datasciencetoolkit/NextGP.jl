a1cd26441e4cc1ca930139d89c2b8a9a8f893f34
Emre Karaman <emrkaraman@gmail.com>
2025-04-17 15:41:43 +0200

    Update MCMC.jl

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -24,7 +24,7 @@ include("outFiles.jl")
 """
 runLMEM = function(model...;nChain=10000,nBurn=1000,nThin=10,myHints=Dict{Symbol,Any}(),blockThese=[],outFolder="outMCMC",VCV=[],userPedData=[],summaryStat=Dict{Any,Any}())
 
-	isa(model(model),Tuple) ? println("I AM A TUPLE") : println("I AM NOT A TUPLE")
+	isa(model,Tuple) ? println("I AM A TUPLE") : println("I AM NOT A TUPLE")
 
 	folderHandler(outFolder)
 
