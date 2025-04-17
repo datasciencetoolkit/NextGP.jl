ba95293e7f29e0de2ab4d7695648e70ed30dde17
Emre Karaman <emrkaraman@gmail.com>
2025-04-17 12:25:32 +0200

    Update MCMC.jl
    model is varargs

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -23,7 +23,7 @@ include("outFiles.jl")
 * `nChain`,`nBurn` and `nThin` are the chain length, burn-in period, and the thining interval used for the MCMC sampling
 * Users can define coding of their variables (e.g. full dummy coding) by providing `myHints`. Check `StatsModels.jl`'s manual for [`categorical data`](https://juliastats.org/StatsModels.jl/latest/contrasts/#Modeling-categorical-data) could be useful.  
 """
-runLMEM = function(model,nChain,nBurn,nThin;myHints=Dict{Symbol,Any}(),blockThese=[],outFolder="outMCMC",VCV=[],userPedData=[],summaryStat=Dict{Any,Any}())
+runLMEM = function(model...;nChain=10000,nBurn=1000,nThin=10,myHints=Dict{Symbol,Any}(),blockThese=[],outFolder="outMCMC",VCV=[],userPedData=[],summaryStat=Dict{Any,Any}())
 	
 	folderHandler(outFolder)
 
