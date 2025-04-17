8aa0f9622fe8a133d563ee2616253daaa0e16eaa
Emre Karaman <emrkaraman@gmail.com>
2025-04-17 15:08:27 +0200

    Update MCMC.jl

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -27,7 +27,7 @@ runLMEM = function(model...;nChain=10000,nBurn=1000,nThin=10,myHints=Dict{Symbol
 
 	println("model is a $(typeof(model))")
 
-	isa(typeof{model},Tuple{Vararg{lmm}}) || isa(typeof{model},Tuple{lmm}) ? println(ALL IS WELL) : throw(ArgumentError("Please enter a valid model"))
+	isa(typeof{model},Tuple{Vararg{lmm}}) || isa(typeof{model},Tuple{lmm}) ? println("ALL IS WELL") : throw(ArgumentError("Please enter a valid model"))
 	
 	isa(model,Tuple{Vararg{lmm}}) || isa(model,Tuple{lmm}) ? nothing : throw(ArgumentError("Please enter a valid model"))
 
