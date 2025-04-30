4fd719b4676432b09f57d63ab54042fb4a6444a9
Emre Karaman <emrkaraman@gmail.com>
2025-04-30 13:37:13 +0200

    Update varComp.jl

--- src/varComp.jl
+++ src/varComp.jl
@@ -35,7 +35,7 @@ end
 
 #set up (co)variance structures for U
 
-function setVarCovStr!(zSet::ExprOrSymbolOrTuple,Z::Dict,priorVCV,varU_prior::Dict,varU::Dict)
+function setVarCovStr!(zSet::ExprOrSymbolOrTuple,Z::Dict,priorVCV,varU::Dict)
 	if haskey(priorVCV,zSet)	
 		if ismissing(priorVCV[zSet].str) || priorVCV[zSet].str=="I" 
 			printstyled("prior var-cov structure for $zSet is either empty or \"I\" was given. An identity matrix will be used\n"; color = :green)
@@ -48,16 +48,13 @@ function setVarCovStr!(zSet::ExprOrSymbolOrTuple,Z::Dict,priorVCV,varU_prior::Di
 			isa(zSet,Tuple) ? Z[zSet][:iVarStr] = Z[zSet[1]][:iVarStr] : Z[zSet][:iVarStr] = Z[zSet][:iVarStr]
 		else 	Z[zSet][:iVarStr] = inv(priorVCV[zSet].str)
 		end
-		varU_prior[zSet] = priorVCV[zSet].v
+		varU[zSet] = priorVCV[zSet].v
 	else	
 		printstyled("prior var-cov for $zSet is empty. An identity matrix will be used with mean=0 and variance=100\n"; color = :green)
-		varU_prior[zSet] = 100
+		varU[zSet] = 100
 		priorVCV[zSet] = Random("I",100)
 		Z[zSet][:iVarStr] = Matrix(1.0I,Z[zSet][:dims][2],Z[zSet][:dims][2])
 	end
-	#for storage
-	varU = deepcopy(varU_prior) #for storage
-	#
 end
 
 #df, shape, scale...															
