5658287a6b9749dac4e79b80d13470a537fcf4c4
Emre Karaman <emrkaraman@gmail.com>
2025-05-06 16:08:15 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -42,7 +42,9 @@ function runSampler!(modelInformation,ycorr,nData,E,varE,X,b,Z,u,varU,M,beta,var
 
 			#[sampleX!(xSet,X,b,ycorr,varE,ySet) for xSet in keys(yModel) if isa(yModel[xSet],DataTerm)]
 			[println("sampling $xSet") for xSet in keys(yModel) if isa(values(yModel[xSet]),DataTerm)]
-			[println("typeof $(yModel[xSet])") for xSet in keys(yModel) if !isa(values(yModel[xSet]),DataTerm)]
+			#[println("typeof $(yModel[xSet])") for xSet in keys(yModel) if !isa(values(yModel[xSet]),DataTerm)]
+			[println("name typeof $(nameof(typeof(yModel[xSet])))") for xSet in keys(yModel)]
+			[println("module typeof $(parentmodule(typeof(yModel[xSet])))") for xSet in keys(yModel)]
 	
 			#sample random effects and variances
 			for zSet in keys(Z)
