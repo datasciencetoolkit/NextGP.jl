d5f003c37729d8cf99bc654f14c3087c65e6132d
Emre Karaman <emrkaraman@gmail.com>
2025-05-06 15:30:57 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -41,7 +41,7 @@ function runSampler!(modelInformation,ycorr,nData,E,varE,X,b,Z,u,varU,M,beta,var
 			#end
 
 			#[sampleX!(xSet,X,b,ycorr,varE,ySet) for xSet in keys(yModel) if isa(yModel[xSet],DataTerm)]
-			[println("sampling $xSet") for xSet in keys(yModel) if isa(yModel[xSet],DataTerm)]
+			[println("sampling $xSet") for xSet in keys(yModel) if isa(yModel[xSet],NextGP.MCMC.prepMatVec.DataTerm)]
 	
 			#sample random effects and variances
 			for zSet in keys(Z)
