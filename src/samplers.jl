f4262b54d3ed1633e93a799bdd26c01345f08d69
Emre Karaman <emrkaraman@gmail.com>
2025-05-07 12:20:38 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -42,7 +42,7 @@ function runSampler!(modelInformation,ycorr,nData,E,varE,X,b,Z,u,varU,M,beta,var
 
 			#[sampleX!(xSet,X,b,ycorr,varE,ySet) for xSet in keys(yModel) if isa(yModel[xSet],DataTerm)]
 			#[println("sampling $xSet") for xSet in keys(yModel) if isa(yModel[xSet],DataTerm)]
-			gh = [typeof(yModel[xSet]) for xSet in keys(yModel) if typeof(yModel[xSet]) <: MCMC.prepMatVec.FixedEffect]
+			gh = [typeof(yModel[xSet]) for xSet in keys(yModel) if typeof(yModel[xSet]) <: FixedEffect]
 			println("GH: $gh")
 			
 			#sample random effects and variances
