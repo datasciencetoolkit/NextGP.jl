8d125a399b294029f4e105543f4bc48f8d6fb668
Emre Karaman <emrkaraman@gmail.com>
2025-05-07 13:11:31 +0200

    Update MCMC.jl

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -17,6 +17,7 @@ include("samplers.jl")
 """
 runLMEM = function(model...;nChain=10000,nBurn=1000,nThin=10,myHints=Dict{Symbol,Any}(),blockThese=Dict{Symbol,Any}(),outFolder="outMCMC",VCV=Dict{Union{Expr,Symbol,Tuple}, Any}(),userPedData=[],summaryStat=Dict{Any,Any}())
 
+	println("MODEL TYPE: $(modelType(model))")
 	#isa(prepMatVec.modelType(model),prepMatVec.lmm) || isa(prepMatVec.modelType(model),Tuple{Vararg{prepMatVec.lmm}}) ? println("I AM A lmm") : println("I AM NOT A lmm")
 	isa(modelType(model),lmm) || isa(modelType(model),Tuple{Vararg{lmm}}) ? println("I AM A lmm") : println("I AM NOT A lmm")
 
