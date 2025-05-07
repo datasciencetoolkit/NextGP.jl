a7c15927188fad3960dd6bdaba8832f72629b6b7
Emre Karaman <emrkaraman@gmail.com>
2025-05-07 13:54:17 +0200

    Update MCMC.jl

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -19,9 +19,9 @@ runLMEM = function(model...;nChain=10000,nBurn=1000,nThin=10,myHints=Dict{Symbol
 
 	println("MODEL: $model")
 	#isa(prepMatVec.modelType(model),prepMatVec.lmm) || isa(prepMatVec.modelType(model),Tuple{Vararg{prepMatVec.lmm}}) ? println("I AM A lmm") : println("I AM NOT A lmm")
-	if length(model)==1 && isa(model.lhs,Symbol)
+	if length(model)==1 && isa(model[1].lhs,Symbol)
 		println("I AM A lmm")
-	elseif length(model)==1 && isa(model.lhs,Expr)
+	elseif length(model)==1 && isa(model[1].lhs,Expr)
 		println("I AM A MV lmm")
 	elseif length(model)>1
 		println("I AM A multi population lmm")
