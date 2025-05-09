078600d7d045e0326b76bcfb9a1d2a6bcf2f80f4
Emre Karaman <emrkaraman@gmail.com>
2025-05-09 12:43:04 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -36,7 +36,7 @@ function sampleb!(xSet::Union{Symbol,Tuple},X::Dict,b::Vector,ycorr::Vector,iVar
 end
 
 # NEW with D and with Wang's Trick
-function sampleX!(xSet::Union{Symbol,Tuple},X::Dict,b::Vector,ycorr::Vector,varE::Dict,ySet::Symbol)
+function sampleX!(xSet::Union{Symbol,Tuple},X::Dict,b,ycorr::Vector,varE::Dict,ySet::Symbol)
 	iVarE = inv(varE[ySet])
 	if X[xSet].nCol==1
 		println("sampling xSet $xSet at POS: $(X[xSet].pos)")
