4a5296415cb982b09b5dc93187a6975e1556ef2a
Emre Karaman <emrkaraman@gmail.com>
2025-05-09 14:30:21 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -105,7 +105,13 @@ function MMEX!(X,b,eSet::Tuple,E,blocks,modelInformation,summaryStat)
 				push!(tempxpX,sum(c.*E[eSet][:iVarStr].*nowX,dims=1))
 			end
 			X[xSet][:Xp] = ones(length(eSet),1) .* map(i -> transpose(nowX[:,i].*E[eSet][:iVarStr]), axes(nowX, 2))
-		#else
+		else
+			for c in eachcol(nowX)
+				#I compute x'X so that for each effect (column), I have both xpx and xpX stored! 
+				push!(tempxpx,Matrix(I*sum(c.*c),length(eSet)))
+				push!(tempxpX,sum(c.*nowX,dims=1))
+			end
+			X[xSet][:Xp] = ones(length(eSet),1) .* map(i -> transpose(nowX[:,i]), axes(nowX, 2))
 		end
 		println("tempxpx: $tempxpx")
 		println("tempxpX: $tempxpX")
