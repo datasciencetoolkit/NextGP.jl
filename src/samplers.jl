04322dd05d8d46d459864e9f62fbbd5195c552c8
Emre Karaman <emrkaraman@gmail.com>
2025-05-13 10:04:57 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -43,7 +43,7 @@ function runSampler!(modelInformation,ycorr,nData,E,varE,X,b,Z,u,varU,M,beta,var
 			#end
 
 			#[println("sampling $xSet") for xSet in keys(yModel) if isa(yModel[xSet],FixedEffect)]
-			[sampleX!(xSet,X,b,ycorr,varE,ySet) for xSet in keys(yModel) if isa(yModel[xSet],FixedEffect)]
+			[sampleX!(xSet,X,b,ycorr,varE,ySet) for xSet in keys(yModel) if isa(yModel[xSet],FixedEffect) && isa(eSet,Symbol)]
 			
 			#sample random effects and variances
 			for zSet in keys(Z)
