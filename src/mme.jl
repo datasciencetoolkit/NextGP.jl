81ca5524af4b50df079aed8fd9ec2bf8ddd2c5d2
Emre Karaman <emrkaraman@gmail.com>
2025-05-13 15:54:15 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -102,7 +102,8 @@ function MMEX!(X,b,posXcounter,eSet::Tuple,E,blocks,modelInformation,summaryStat
 		push!(b,[zeros(Float64,1,length(eSet)) for j in 1:X[xSet][:nCol]]) #is an array of arrays also!
 						
 		#Matrix of matrixces	
-		X[xSet][:XpX] = hcat([[x'*tempX[j] for j in 1:length(tempX)] for x in tempX]...)
+		#X[xSet][:XpX] = hcat([[x'*tempX[j] for j in 1:length(tempX)] for x in tempX]...) #returns a big matrix of k'k!
+		X[xSet][:XpX] = [[x'*tempX[j] for j in 1:length(tempX)] for x in tempX] #returns k array of arrays of t*t!
 		println("X[xSet][:XpX]: $(X[xSet][:XpX])")
 		#X[xSet][:XpX] = [reduce(hcat, X[xSet][:XpX][i,:]) for i in 1:X[xSet][:nCol]]
 		#X[xSet][:Xp]  = transpose.(tempX)
