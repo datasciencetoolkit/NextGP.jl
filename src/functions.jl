508813d107f7aeeb911a06a067e858bfb3ce3dd1
Emre Karaman <emrkaraman@gmail.com>
2025-05-15 22:14:46 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -42,13 +42,8 @@ function sampleb!(xSet::Union{Symbol,Tuple},X::Dict,b::Vector,ycorr::Matrix,iVar
 		Yj = [BLAS.dot(X[xSet].data[j][:,t],sum(ycorr .* iVarE[[t],:],dims=2)) for t in 1:size(ycorr,2)]
         	bVec[j] .= 0.0 #also excludes the effect from iMat! Nice trick.
 		foreach(k -> push!(rhsj0,k.*iVarE), X[xSet].XpX[[j],:])
-		println("size rhsj0: $(hcat(rhsj0...))")
-		println("size vec(hcat(bVec...) $(vec(hcat(bVec...)))")
 		rr = hcat(rhsj0...)*vec(hcat(bVec...))
-		println(rr)
-		println(Yj)
 		rhsj = Yj - rr
-		#rhsj = Yj - sum(rhsj0.*bVec')
                 lhsj = getindex(X[xSet].XpX,j,j).*iVarE
 		invLhsj = inv(lhsj)
                 meanj = invLhsj*rhsj
