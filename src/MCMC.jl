f2b6da999e75ea067c26030db8cd403cdabc4b25
Emre Karaman <emrkaraman@gmail.com>
2025-05-30 14:01:24 +0200

    Update MCMC.jl

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -32,7 +32,7 @@ runLMEM = function(model...;nChain=10000,nBurn=1000,nThin=10,myHints=Dict{Symbol
 	folderHandler(outFolder)
 
 	#Y,X,Z,M,E,modelInformation = prepMatVec.prep(model,priorVCV=VCV)
-	Y,X,Z,M,E,modelInformation = prep(model,path2ped=userPedData,priorVCV=VCV)
+	Y,X,Z,M,E,modelInformation = prep(model,priorVCV=VCV)
 
 	modelInformation,ycorr,nData,E,varE,X,b,Z,u,varU,M,beta,varBeta,delta = getMME!(Y,X,Z,M,E,blockThese,VCV,summaryStat,modelInformation,outFolder)
 
