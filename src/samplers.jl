d69fcc2f387be1673f64e6cd563adfaba7e30f9f
Emre Karaman <emrkaraman@gmail.com>
2025-06-06 12:55:36 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -37,14 +37,15 @@ function runSampler!(modelInformation,ycorr,nData,E,varE,X,b,Z,u,varU,M,beta,var
 			#sample fixed effects
 			[sampleX!(xSet,X,b,ycorr,varE,ySet) for xSet in keys(yModel) if isa(yModel[xSet],FixedEffect)] 
 
-
+			#sample random effects
+			[sampleZ!(zSet,Z,u,ycorr,varE,ySet,varU) for zSet in keys(yModel) if isa(yModel[zSet],RandomEffect)] 
 
 
 
 			
 			#sample random effects and variances
 			for zSet in keys(Z)
-	        		sampleZ!(zSet,Z,u,ycorr,varE,varU)	
+	        		sampleZ!(zSet,Z,u,ycorr,varE,ySet,varU)	
 			end
 		
 	
