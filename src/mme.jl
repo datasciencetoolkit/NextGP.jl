2ddda6798aaff7ebe2c5b2509dbc4c4506d08f91
Emre Karaman <emrkaraman@gmail.com>
2025-06-11 10:37:53 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -588,8 +588,8 @@ function getMME!(Y,X,Z,M,E,blocks,priorVCV,summaryStat,modelInformation,outPut) 
 #	end	
 
 	#need to adapt model related Z's, below does it for all Z for all equation! Check what E has and what is eSEt
-	[[inOut.outMCMC(outPut,"u$zSet$eSet",[Z[zSet][:levels]]) for zSet in keys(Z) if isa(zSet,Union{Symbol,Expr})] for eSet in keys(E) if isa(eSet,Symbol)] #single trait	
-	[[[inOut.outMCMC(outPut,"u$zSet$eSet",[Z[zSet][:levels]]) for z in zSet] for zSet in keys(Z) if isa(zSet,Tuple)] for eSet in keys(E) if isa(eSet,Tuple)] #single-trait multiple comp
+	[[inOut.outMCMC(outPut,"u$zSet$eSet",[Z[zSet][:levels]]) for zSet in keys(Z) if (isa(zSet,Union{Symbol,Expr}) && in(zSet,modelInformation[eSet]))] for eSet in keys(E) if isa(eSet,Symbol)] #single trait	
+	[[[inOut.outMCMC(outPut,"u$zSet$eSet",[Z[zSet][:levels]]) for z in zSet] for zSet in keys(Z) if (isa(zSet,Tuple) && in(zSet,modelInformation[eSet]))] for eSet in keys(E) if isa(eSet,Tuple)] #single-trait multiple comp
 
 	
 	[(inOut.outMCMC(outPut,"varU$zSet",[String(zSet)]) for zSet in keys(Z) if isa(zSet,Symbol)) for eSet in keys(E) if isa(eSet,Symbol)] #[] to have it as one row
