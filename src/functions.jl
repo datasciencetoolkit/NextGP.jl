0ee23782d2a947bc89889ce53a087eaa3976bf7b
Emre Karaman <emrkaraman@gmail.com>
2025-06-12 10:52:08 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -107,8 +107,10 @@ function sampleU(zSet::Tuple,Z::Dict,iVarE::Float64,varU::Dict,u::Vector,ycorr::
 	iVarU = inv(varU[zSet])
 	for i in 1:nCol
 		setindex!(uVec,[0;0],:,i)
-		Yi = Z[zSet].Zp[i]*ycorr		
-		rhsU = (Yi.*iVarE) - kron(view(Z[zSet].iVarStr,[i],:),iVarU)*vec(uVec)
+		println("Z[zSet].Zp: $(Z[zSet].Zp[i])")
+		println("Z[zSet].Zp[i]: $(Z[zSet].Zp[i])")
+		Yi = Z[zSet].Zp[i]*ycorr.*iVarE		
+		rhsU = Yi - kron(view(Z[zSet].iVarStr,[i],:),iVarU)*vec(uVec)
                 invLhsU = inv((getindex(Z[zSet].zpz,i).*iVarE) + (view(Z[zSet].iVarStr,i,i).*iVarU))
                 meanU = invLhsU*rhsU
 		setindex!(uVec,rand(MvNormal(meanU,convert(Array,Symmetric(invLhsU)))),:,i)
