c454b431d584dab1702b9ea5decf9dcd17fbf046
Emre Karaman <emrkaraman@gmail.com>
2025-06-21 22:33:54 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -353,8 +353,15 @@ function MMEM!(M,beta,varBeta,delta,posMcounter,eSet::Tuple,E,priorVCV,modelInfo
 	println("priorVCV: $priorVCV")
 	#correlate = hcat(filter!(!isempty, unique([[keya for (keya,valuea) in priorVCV if ((isa(keya,Tuple) || isa(valuea.v,Matrix{Float64})) && all(in.(keyz,keya)))] for keyz in keys(M)]))...)
 	#Alternative as the above gives strange error I cannot produce
-	correlate = hcat(filter!(!isempty, unique([[keya for (keya,valuea) in priorVCV if ((isa(keya,Tuple) || isa(valuea.v,Matrix{Float64})) && in(keyz,keya))] for keyz in keys(M)]))...)
-
+	#correlate = hcat(filter!(!isempty, unique([[keya for (keya,valuea) in priorVCV if ((isa(keya,Tuple) || isa(valuea.v,Matrix{Float64})) && in(keyz,keya))] for keyz in keys(M)]))...)
+	for keym in keys(M)
+		for (keya,valuea) in priorVCV 
+			if ((isa(keya,Tuple) || isa(valuea.v,Matrix{Float64}) && all(in.(keyz,keya))))
+				println("$keyz is in $keya")
+			end
+		end
+		
+	end
 
 	println("correlate $correlate")
 
