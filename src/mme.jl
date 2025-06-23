a18c0016584e198060101e446b538d735710ac9b
Emre Karaman <emrkaraman@gmail.com>
2025-06-24 01:58:21 +0300

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -643,7 +643,7 @@ function getMME!(Y,X,Z,M,E,blocks,priorVCV,summaryStat,modelInformation,outPut) 
 	isempty(blocks) ? levelsX = hcat(vcat([value[:levels] for (key, value) in X]...)...) : levelsX = hcat(vcat([vcat(value[:levels]) for (key, value) in X]...)...)
 
 	[inOut.outMCMC(outPut,"b_$eSet",levelsX) for eSet in keys(E) if isa(eSet,Symbol)]
-	[inOut.outMCMC(outPut,"b_$eSet",hcat(["$(l)_".*String.(hcat(eSet...)) for l in levelsX]...)) for eSet in keys(E) if isa(eSet,Tuple{Vararg{Symbol}})]
+	[inOut.outMCMC(outPut,"b_"*join(mSet, "_"),hcat(["$(l)_".*String.(hcat(eSet...)) for l in levelsX]...)) for eSet in keys(E) if isa(eSet,Tuple{Vararg{Symbol}})]
 	
 	
 	#check for correlated RE
@@ -679,7 +679,6 @@ function getMME!(Y,X,Z,M,E,blocks,priorVCV,summaryStat,modelInformation,outPut) 
 	#beta
 	[[inOut.outMCMC(outPut,"beta$mSet$eSet",[M[mSet][:levels]]) for mSet in keys(M) if (isa(mSet,Symbol) && in(mSet,keys(modelInformation[eSet])))] for eSet in keys(E) if isa(eSet,Symbol)] #single trait
 	[[[inOut.outMCMC(outPut,"beta$m$eSet",[M[mSet][:levels]]) for m in mSet] for mSet in keys(M) if (isa(mSet,Tuple) && in(mSet,keys(modelInformation[eSet])))] for eSet in keys(E) if isa(eSet,Symbol)] #single trait multi comp
-        println("ESET is: $([eSet for eSet in keys(E)])")
 	[[[inOut.outMCMC(outPut,"beta$m$(eSet[p])",beta[M[mSet][:pos]][[p],:]) for (p,m) in enumerate(mSet)] for mSet in keys(M) if (isa(mSet,Tuple{Vararg{Symbol}}))] for eSet in keys(E) if isa(eSet,Tuple)] #multi-trait only one correlated comp (:M1,M1) or (:M1,:M2...) but not ((:M1,:M2) and (:M3,:M4))	
 	#[[[println("p $p and m $m") for (p,m) in enumerate(mSet)] for mSet in keys(M) if (isa(mSet,Tuple{Vararg{Symbol}}))] for eSet in keys(E) if isa(eSet,Tuple)] #multi-trait only one correlated comp (:M1,M1) or (:M1,:M2...) but not ((:M1,:M2) and (:M3,:M4))	
 
@@ -724,7 +723,7 @@ function getMME!(Y,X,Z,M,E,blocks,priorVCV,summaryStat,modelInformation,outPut) 
 
 	
 	[inOut.outMCMC(outPut,"varE_$eSet",["e_$eSet"]) for eSet in keys(E) if isa(eSet,Symbol)]
-	[inOut.outMCMC(outPut,"varE_$eSet",hcat(["$(c[1])_$(c[2])" for c in collect(Iterators.product(1:length(eSet), 1:length(eSet)))]...)) for eSet in keys(E) if isa(eSet,Tuple{Vararg{Symbol}})]
+	[inOut.outMCMC(outPut,"varE_"*join(eSet, "_"),hcat(["$(c[1])_$(c[2])" for c in collect(Iterators.product(1:length(eSet), 1:length(eSet)))]...)) for eSet in keys(E) if isa(eSet,Tuple{Vararg{Symbol}})]
 
 	
 	##########
