b04b6940e650f71e129f6496966fb9dfa283fb19
Emre Karaman <emrkaraman@gmail.com>
2025-06-25 23:00:34 +0300

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -3,7 +3,7 @@ abstract type RandomEffect end
 abstract type RandomGeneticEffect <: RandomEffect end
 abstract type RandomMarkerEffect <: RandomEffect end
 
-PiTypes = Union{Matrix{Float64},Vector{Float64},Float64} #pi can be different (vector) or same per SNP (NO COR BayesB YET). BayesR also takes a vector of pi
+PiTypes = Union{Vector{Float64},Float64} #pi can be different (vector) or same per SNP (NO COR BayesB YET). BayesR also takes a vector of pi
 VarCovarTypes = Union{Vector{Matrix{Float64}},Vector{Float64},Matrix{Float64},Float64} #prior for (co)var can be different (vector) or same per SNP
 ExprOrSymbol = Union{Expr,Symbol}
 ExprOrSymbolOrTuple = Union{Expr,Symbol,Tuple}
