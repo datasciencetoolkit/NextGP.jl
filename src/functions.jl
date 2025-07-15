fc3b427b97b5dba05649cfe2b6bfe1a1846b2c7b
Emre Karaman <emrkaraman@gmail.com>
2025-07-15 10:10:06 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -228,7 +228,7 @@ function sampleBayesB!(mSet::Symbol,M::OrderedDict,beta::Vector,delta::Vector,yc
 	local lambda::Float64
 	nLoci = 0
 	for (r,theseLoci) in enumerate(M[mSet].regionArray) #theseLoci is always as 1:1,2:2 for BayesB, so r=locus
-		iVarE = 1/varE
+		iVarE = 1/varE[ySet]
 		iVarBeta = 1/varBeta[mSet][r]
 		for locus in theseLoci::UnitRange{Int64}
 			BLAS.axpy!(getindex(beta[M[mSet].pos],locus),view(M[mSet].data,:,locus),ycorr)
@@ -329,7 +329,7 @@ function sampleBayesC!(mSet::Symbol,M::OrderedDict,beta::Vector,delta::Vector,yc
 	local meanBeta::Float64
 	local lambda::Float64
 	nLoci = 0
-	iVarE = 1/varE
+	iVarE = 1/varE[ySet]
 	iVarBeta = 1/varBeta[mSet][1]
 	for (r,theseLoci) in enumerate(M[mSet].regionArray) #theseLoci is always as 1:1,2:2 for BayesC, so r=locus
 		for locus in theseLoci::UnitRange{Int64}
