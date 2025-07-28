8689ff8efae817736fcfc3f13a6397aabc196fa1
Emre Karaman <emrkaraman@gmail.com>
2025-07-28 07:48:07 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -290,7 +290,8 @@ function sampleBayesB!(mSet::Tuple,M::OrderedDict,beta::Vector,delta::Vector,yco
 				end
 			end
 
-			tempGammaProb = zeros(size(M[mSet].gammaComb,1)) #to store prob
+			tempGammaProb = zeros(length(M[mSet].gammaComb)) #to store prob
+			println("tempGammaProb: $tempGammaProb")
 			
 			for thisGamma in 1:length(M[mSet].gammaComb)
                			C = M[mSet].deltaComb[thisGamma]*getindex(M[mSet].mpm,locus)*M[mSet].deltaComb[thisGamma] .+ (iVarBeta.*varE[ySet])
