1ce95d3bc5acd2a7affc90779351d4f202c43dbb
Emre Karaman <emrkaraman@gmail.com>
2025-07-28 10:11:14 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -330,7 +330,7 @@ function sampleBayesB!(mSet::Tuple,M::OrderedDict,beta::Vector,delta::Vector,yco
 				end
 			end
 		end
-		@inbounds varBeta[mSet][r] = sampleVarCovBetaPR(M[mSet].scale[],M[mSet].df[],getindex(beta[M[mSet].pos],:,theseLoci),1)
+		@inbounds varBeta[mSet][r] = sampleVarCovBetaPR(M[mSet].scale,M[mSet].df[],getindex(beta[M[mSet].pos],:,theseLoci),1)
 	end
 	if M[mSet].estPi == true 
 		M[mSet].piHat .= rand(Dirichlet(storeCount.+1))
