1e8887390112d8480b413006f396970720183f84
Emre Karaman <emrkaraman@gmail.com>
2025-08-29 14:33:56 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -276,11 +276,10 @@ function sampleBayesB!(mSet::Symbol,M::OrderedDict,beta::Vector,delta::Vector,yc
 		M[mSet].logPi .= log.([1.0-piIn piIn])
 	end
 	if (M[mSet].params==true) && (length(varBeta[mSet]) > 1)
-		inMarkers = findall(>(0),delta[M[mSet].pos])
-		inMarkers = getindex.(inMarkers,2)
-		println("n markers: $(length(inMarkers))")
-		dfIG,scaleIG = sampleScaleDFofVar(getindex(varBeta[mSet],inMarkers))
-		#dfIG,scaleIG = sampleScaleDFofVar(varBeta[mSet])
+		#inMarkers = findall(>(0),delta[M[mSet].pos])
+		#inMarkers = getindex.(inMarkers,2)
+		#dfIG,scaleIG = sampleScaleDFofVar(getindex(varBeta[mSet],inMarkers))
+		dfIG,scaleIG = sampleScaleDFofVar(varBeta[mSet])
 		dfScaleInvChi = 2*dfIG
 		scaleScaleInvChi = scaleIG/dfIG
 		setindex!(M[mSet].scale, scaleScaleInvChi,1)
@@ -811,7 +810,6 @@ function sampleScaleOfVar(df,var,k)
 end
 
 function sampleScaleDFofVar(varVec;abcde=[4.0,0,01,0.01,0.01,0.01])
-	println("length(varVec:): $(length(varVec))")
 	a,b,c,d,e = abcde
 	n = length(varVec)
 	mu = mean(varVec)
