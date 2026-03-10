81156093c4c73118cf3467825f39d74530e3b81c
Emre Karaman <emrkaraman@gmail.com>
2026-03-10 10:03:13 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -444,7 +444,8 @@ function sampleBayesC!(mSet::Tuple,M::OrderedDict,beta::Vector,delta::Vector,yco
 			meanBETA = vec(invLHS*RHS)			
 			setindex!(beta[M[mSet].pos],rand(MvNormal(meanBETA,convert(Array,Symmetric(invLHS)))),:,locus)
 			println("delta for this locus: $(M[mSet][:deltaHat][locus])")
-			setindex!(delta[M[mSet].pos],M[mSet][:deltaHat][locus],:,locus)
+			println("gamma for this locus: $(M[mSet][:gammaHat][locus])")
+			setindex!(delta[M[mSet].pos],M[mSet][:gammaHat][locus],:,locus)
 
 			if isa(mSet,Tuple{Vararg{Symbol}}) #could also be Tuple{Vararg{Tuple{Vararg{Symbol}}}} which i will adapt later
 				#for i in 1:length(ySet)
