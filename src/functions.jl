07bc72641a7e645ddece2236012fa8254273401f
Emre Karaman <emrkaraman@gmail.com>
2026-03-12 11:57:32 +0100

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -364,6 +364,7 @@ function sampleBayesB!(mSet::Tuple,M::OrderedDict,beta::Vector,delta::Vector,yco
 	elseif (M[mSet].params==true) && (length(varBeta[mSet]) < 2)
 	else 
 		inMarkers = findall([any(c.!=0) for c in eachcol(M[mSet][:gammaHat])])
+		println("inMarkers: $(inMarkers)")
 		println("N inmarkers = $(length(inMarkers))")
 	end
 end
