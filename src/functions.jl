a6aef66b114605331b83dc5d0ce9567c33e25b6a
Emre Karaman <emrkaraman@gmail.com>
2026-03-12 09:49:05 +0100

    Refactor marker finding and update df assignment

--- src/functions.jl
+++ src/functions.jl
@@ -350,13 +350,14 @@ function sampleBayesB!(mSet::Tuple,M::OrderedDict,beta::Vector,delta::Vector,yco
 
 		tempdf = 0
 		for m in 1:length(mSet)
-			inMarkers = findall(>(0),getindex.(M[mSet][:gammaHat],m))
-			#inMarkers = getindex.(inMarkers,2)
+			inMarkers = findall(>(0),getindex(beta[M[mSet].pos],m,:))
+			println("in markers: $inMarkers")
+			inMarkers = getindex.(inMarkers,2)
 			dfIG,scaleIG = sampleScaleDFofVar(getindex.(getindex(varBeta[mSet],inMarkers),m,m))
 			dfScaleInvChi = 2*dfIG
 			tempdf += dfScaleInvChi+1
 		end
-		setindex!(M[mSet].df, rand(3.0:5.0),1)
+		setindex!(M[mSet].df, 4,1)
 
 	elseif (M[mSet].params==true) && (length(varBeta[mSet]) < 2)
 	else nothing
