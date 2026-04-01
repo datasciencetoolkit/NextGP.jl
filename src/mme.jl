c1b7b3c7144986b9bc9a5ae6d4b99573c54224b8
Emre Karaman <emrkaraman@gmail.com>
2026-04-01 14:51:47 +0200

    Add type logging for zSet and Z elements
    Log the type of elements in Z along with zSet.

--- src/mme.jl
+++ src/mme.jl
@@ -200,7 +200,7 @@ function MMEZ!(Z,u,varU,posZcounter,eSet::Symbol,E,priorVCV,modelInformation,sum
 	end
 
 	for zSet in keys(Z)
-		println("typeof $(typeof(zSet))")
+		println("typeof $(typeof(Z[zSet]))")
 		setVarCovStrU!(eSet,zSet,Z,priorVCV,varU)
 	end
 	
