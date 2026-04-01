050c9cd503d7869b099551cdd3a41892c7329ba0
Emre Karaman <emrkaraman@gmail.com>
2026-04-01 15:45:59 +0200

    Update prepMatVec.jl

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -145,7 +145,7 @@ function prep(f;path2ped=[],priorVCV=[]) ### THE REST OF THE CODE FOR XZM SHOUld
 			
 			if isa(priorVCV[k],GBLUPType)
 				Ginv = inv(makeG(thisM;method=priorVCV[k].methodG))
-				Z[k] = Dict(:data=>Matrix(1.0*I,size(thisM,1),size(thisM,1)),:map=>nowMap,:method=>"GBLUP",:str=>"G",:iVarStr=>Ginv,:dims=>size(Ginv),:levels=>["Ind$i" for i in 1:size(thisM,2)]) 	
+				Z[k] = Dict(:data=>Matrix(1.0*I,size(thisM,1),size(thisM,1)),:map=>nowMap,:method=>"GBLUP",:str=>"G",:iVarStr=>Ginv,:dims=>size(Ginv),:levels=>["Ind$i" for i in 1:size(Ginv,2)]) 	
 				push!(summarize,[k,"GBLUP",typeof(Ginv),size(Ginv,2)])
 			else
 				thisM .-= mean(thisM,dims=1)
