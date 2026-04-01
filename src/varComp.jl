6ca6cc9370d9802d6417d92c6cebc6ebfa97ce52
Emre Karaman <emrkaraman@gmail.com>
2026-04-01 15:07:04 +0200

    Update varComp.jl

--- src/varComp.jl
+++ src/varComp.jl
@@ -77,20 +77,21 @@ end
 function setVarCovStrU!(eSet::Symbol,zSet::Union{Symbol,Tuple{Vararg{Symbol}}},Z::OrderedDict,priorVCV,varU::Dict)
 	println("SETTING VARCOV FOR $zSet")
 	println("priorVCV: $priorVCV")
-	if haskey(priorVCV,zSet)	
+	if  haskey(priorVCV,zSet) && isa(RandomEffectType,priorVCV[zSet])
+		println("priorVCV haskey $zSet and is a RandomEffectType")
 		if isempty(priorVCV[zSet].str) || priorVCV[zSet].str=="I" 
 			printstyled("prior var-cov structure for $zSet is either empty or \"I\" was given. An identity matrix will be used\n"; color = :green)
 			Z[zSet][:iVarStr] = Matrix(1.0I,Z[zSet][:dims][2],Z[zSet][:dims][2])
 		elseif priorVCV[zSet].str=="A"
 			printstyled("prior var-cov structure for $zSet is A. Computed A matrix (from pedigree file) will be used\n"; color = :green)
 			#isa(zSet,Tuple) ? Z[zSet][:iVarStr] = Z[zSet[1]][:iVarStr] : Z[zSet][:iVarStr] = Z[zSet][:iVarStr]
-		elseif Z[zSet].str=="G"
-            printstyled("prior var-cov structure for $zSet is G. Computed G matrix will be used\n"; color = :green)
-			#isa(zSet,Tuple) ? Z[zSet][:iVarStr] = Z[zSet[1]][:iVarStr] : Z[zSet][:iVarStr] = Z[zSet][:iVarStr]
 		else 	
 			error("provide a valid prior var-cov structure (\"I\", \"D\" or leave it empty \"[]\") for \"e\" ")
 		end
-	#elseif 
+	elseif haskey(priorVCV,zSet) && isa(GBLUPType,priorVCV[zSet])
+		println("priorVCV haskey $zSet and is a GBLUPType")
+		printstyled("prior var-cov structure for $zSet is G. Computed G matrix will be used\n"; color = :green)
+		#isa(zSet,Tuple) ? Z[zSet][:iVarStr] = Z[zSet[1]][:iVarStr] : Z[zSet][:iVarStr] = Z[zSet][:iVarStr]
 	else	
 		printstyled("prior var-cov for $zSet is empty. An identity matrix will be used with mean=0 and variance=100\n"; color = :green)
 		Z[zSet][:str] = "I"
