dfae85b29fdc5dd686d691ac7d883712961ddef6
Emre Karaman <emrkaraman@gmail.com>
2026-04-01 12:56:53 +0200

    Update prepMatVec.jl

--- src/prepMatVec.jl
+++ src/prepMatVec.jl
@@ -145,8 +145,6 @@ function prep(f;path2ped=[],priorVCV=[]) ### THE REST OF THE CODE FOR XZM SHOUld
 			
 			if isa(priorVCV[k],GBLUPType)
 				Ginv = inv(makeG(thisM;method=priorVCV[k].methodG))
-				println("typeof Ginv: $(typeof(Ginv))")
-				println("Ginv[1:5,1:5]: $(Ginv[[1:5,1:5]])")
 				Z[k] = Dict(:data=>Matrix(1.0*I,size(thisM,1),size(thisM,1)),:map=>nowMap,:method=>"GBLUP",:str=>"G",:iVarStr=>Ginv,:dims=>size(Ginv),:levels=>:levels=>["Ind$i" for i in 1:size(thisM,2)]) 	
 				push!(summarize,[k,"GBLUP",typeof(Ginv),size(Ginv,2)])
 			else
