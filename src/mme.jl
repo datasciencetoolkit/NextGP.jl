186a1a89b85860d73f2150cc9398e3cc207eeb61
Emre Karaman <emrkaraman@gmail.com>
2026-04-01 16:37:35 +0200

    Update mme.jl

--- src/mme.jl
+++ src/mme.jl
@@ -673,7 +673,8 @@ function getMME!(Y,X,Z,M,E,blocks,priorVCV,summaryStat,modelInformation,outPut) 
 	
 	#this prints all u into one file ID_in1,Dam_ind1,ID_ind2,Dam_ind2.... order
 	[[inOut.outMCMC(outPut,"u"*join([String.(zSet)...])*"$eSet",hcat(vcat([Array([String.(zSet)...].*i) for i in Z[zSet][:levels]]...)...)) for zSet in keys(Z) if (isa(zSet,Tuple) && in(zSet,keys(modelInformation[eSet])))] for eSet in keys(E) if isa(eSet,Symbol)] #single-trait multiple comp
-	
+
+	[[inOut.outMCMC(outPut,"varU$zSet",[String(zSet)]) for zSet in keys(Z) if (isa(zSet,Union{Symbol,Expr}) && in(zSet,keys(modelInformation[eSet])))] for eSet in keys(E) if isa(eSet,Symbol)] #single trait	
 	
 	#[(inOut.outMCMC(outPut,"varU$zSet",[String(zSet)]) for zSet in keys(Z) if isa(zSet,Symbol)) for eSet in keys(E) if isa(eSet,Symbol)] #[] to have it as one row
 	#[(inOut.outMCMC(outPut,"varU$zSet",[join(zSet.args)[2:end]]) for zSet in keys(Z) if isa(zSet,Expr)) for eSet in keys(E) if isa(eSet,Symbol)] #[] to have it as one row	
