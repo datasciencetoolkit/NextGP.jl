650bd4304b3a7fce4ad3c765737dae19bfcbecaf
Emre Karaman <emrkaraman@gmail.com>
2026-04-02 10:13:41 +0200

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -116,7 +116,9 @@ struct GBLUPType <: RandomEffect
 	methodG::Int #specific method 0, 1 or 2?
 	G::Union{Matrix{Float64},String}
 end
-GBLUP(v::Float64;w::Vector{Float64}=Float64[],methodG::Int=1,G::Union{Matrix{Float64},String}=[]) = GBLUPType(v,w,methodG,G)
+GBLUP(v::Float64;w::Vector{Float64}=Float64[],methodG::Int=1,G::String="") = GBLUPType(v,w,methodG,G)
+GBLUP(v::Float64;w::Vector{Float64}=Float64[],methodG::Int=1,G::Matrix{Float64}=Matrix{Float64}()) = GBLUPType(v,w,methodG,G)
+
 
 struct BayesPRType <: RandomMarkerEffect
     r::Int
