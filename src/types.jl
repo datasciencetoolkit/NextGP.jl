f28dc4cd5afeaa795088cc7eeab77ab0d2833ff1
Emre Karaman <emrkaraman@gmail.com>
2026-04-02 11:16:22 +0200

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -118,7 +118,7 @@ end
 * `methodG` is the vanRaden methods (1 and 2) for calculating G matrix, or simply G=MM'/k if "0"
 """
 #GBLUP(v::Float64,w::Vector{Float64}=Float64[],methodG::Int=1,G::String="") = GBLUPType(v,w,methodG,G)
-GBLUP(v::Float64,w::Vector{Float64}=Float64[],methodG::Int=1,G::Matrix{Float64}=Matrix{Float64}(undef,0,0)) = GBLUPType(v,w,methodG,G)
+GBLUP(v::Float64;w::Vector{Float64}=Float64[],methodG::Int=1,G::Matrix{Float64}=Matrix{Float64}(undef,0,0)) = GBLUPType(v,w,methodG,G)
 
 
 struct BayesPRType <: RandomMarkerEffect
