6e6c65ca4c30402528c70f712a0522c2ab6221aa
Emre Karaman <emrkaraman@gmail.com>
2026-04-11 11:21:57 +0200

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -767,7 +767,8 @@ end
 #sample random effects' variances (new U)
 function sampleVarU(iMat,scale_ranVar,df_ranVar,effVec)
 	n = size(iMat,2)
-	return ((effVec*iMat*effVec')[])/rand(Chisq(df_ranVar + n)) #(scale_ranVar*df_ranVar + (effVec*iMat*effVec')[])/rand(Chisq(df_ranVar + n))
+	df_ranVar = 0.0 ################
+	return (scale_ranVar*df_ranVar + (effVec*iMat*effVec')[])/rand(Chisq(df_ranVar + n))
 end
 
 function sampleCoVarU(iMat,scale_ranVar,df_ranVar,effVec)
