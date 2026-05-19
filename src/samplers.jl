a51913fde7ee60e3d7cccb67f5fa8cf77a0a6a44
Emre Karaman <emrkaraman@gmail.com>
2026-05-19 11:04:22 +0200

    Update samplers.jl

--- src/samplers.jl
+++ src/samplers.jl
@@ -72,6 +72,9 @@ function runSampler!(modelInformation,ycorr,nData,E,varE,X,b,Z,u,varU,M,beta,var
 				[[[inOut.outMCMC(outPut,"delta$m$(eSet[p])",delta[M[mSet].pos][[p],:]) for (p,m) in enumerate(mSet)] for mSet in keys(M) if (isa(mSet,Tuple{Vararg{Symbol}}))] for eSet in keys(E) if isa(eSet,Tuple)] #multi-trait only one correlated comp (:M1,M1) or (:M1,:M2...) but not ((:M1,:M2) and (:M3,:M4))
 				#[[[inOut.outMCMC(outPut,"delta$m$(eSet[p])",getindex.(M[mSet][:gammaHat],p)') for (p,m) in enumerate(mSet)] for mSet in keys(M) if (isa(mSet,Tuple{Vararg{Symbol}}))] for eSet in keys(E) if isa(eSet,Tuple)] #multi-trait only one correlated comp (:M1,M1) or (:M1,:M2...) but not ((:M1,:M2) and (:M3,:M4))
 
+				#currently only one component with annotations
+				#[[inOut.outMCMC(outPut,"c$mSet$eSet",c[M[mSet].pos]) for mSet in keys(M) if (isa(mSet,Symbol) && in(mSet,keys(modelInformation[eSet])))] for eSet in keys(E) if isa(eSet,Symbol)] #single trait	
+				[[inOut.outMCMC(outPut,"c$mSet$eSet",c) for mSet in keys(M) if (isa(mSet,Symbol) && in(mSet,keys(modelInformation[eSet])))] for eSet in keys(E) if isa(eSet,Symbol)] #single trait	
 
 				#for mSet in keys(M)
 				#	if isa(mSet,Symbol)
